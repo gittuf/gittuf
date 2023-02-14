c94f87d5f92e425731d419c4c146fb329e681765
Aditya Sirish <aditya@saky.in>
2023-02-14 16:30:53 -0500

    Add gitinterface package
    This package includes a set of helpers to interface with the underlying
    git repository. Features:
    * parse git configs to determine commit signing mechanism
    * create and sign commit objects
    
    Signed-off-by: Aditya Sirish <aditya@saky.in>

---
+++ internal/gitinterface/commit.go
@@ -0,0 +1,146 @@
+package gitinterface
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/config"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/jonboulle/clockwork"
+)
+
+var ErrUnableToSign = errors.New("unable to sign Git object")
+
+func Commit(repo *git.Repository, treeHash plumbing.Hash, targetRef string, message string, sign bool) error {
+	gitConfig, err := repo.ConfigScoped(config.GlobalScope)
+	if err != nil {
+		return err
+	}
+
+	curRef, err := repo.Reference(plumbing.ReferenceName(targetRef), true)
+	if err != nil {
+		return err
+	}
+
+	commit := createCommitObject(gitConfig, treeHash, curRef.Hash(), message, clockwork.NewRealClock())
+
+	if sign {
+		command, args, err := GetSigningCommand(repo)
+		if err != nil {
+			return err
+		}
+		signature, err := signCommit(repo, commit, command, args)
+		if err != nil {
+			return err
+		}
+		commit.PGPSignature = signature
+	}
+
+	obj := repo.Storer.NewEncodedObject()
+	err = commit.Encode(obj)
+	if err != nil {
+		return err
+	}
+	commitHash, err := repo.Storer.SetEncodedObject(obj)
+	if err != nil {
+		return err
+	}
+
+	newRef := plumbing.NewHashReference(plumbing.ReferenceName(targetRef),
+		commitHash)
+	return repo.Storer.CheckAndSetReference(newRef, curRef)
+}
+
+func createCommitObject(gitConfig *config.Config, treeHash plumbing.Hash, parentHash plumbing.Hash, message string, clock clockwork.Clock) *object.Commit {
+	author := object.Signature{
+		Name:  gitConfig.User.Name,
+		Email: gitConfig.User.Email,
+		When:  clock.Now(),
+	}
+
+	commit := &object.Commit{
+		Author:    author,
+		Committer: author,
+		TreeHash:  treeHash,
+		Message:   message,
+	}
+	if !parentHash.IsZero() {
+		commit.ParentHashes = []plumbing.Hash{parentHash}
+	}
+
+	return commit
+}
+
+func signCommit(repo *git.Repository, commit *object.Commit, signingCommand string, signingArgs []string) (string, error) {
+	commitEncoded := repo.Storer.NewEncodedObject()
+	if err := commit.EncodeWithoutSignature(commitEncoded); err != nil {
+		return "", err
+	}
+	r, err := commitEncoded.Reader()
+	if err != nil {
+		return "", err
+	}
+	commitContents, err := io.ReadAll(r)
+	if err != nil {
+		return "", err
+	}
+
+	cmd := exec.Command(signingCommand, signingArgs...)
+
+	stdInWriter, err := cmd.StdinPipe()
+	if err != nil {
+		return "", err
+	}
+
+	stdOutReader, err := cmd.StdoutPipe()
+	if err != nil {
+		return "", err
+	}
+	defer stdOutReader.Close()
+
+	stdErrReader, err := cmd.StderrPipe()
+	if err != nil {
+		return "", err
+	}
+	defer stdErrReader.Close()
+
+	if err = cmd.Start(); err != nil {
+		return "", err
+	}
+
+	if _, err := stdInWriter.Write(commitContents); err != nil {
+		return "", err
+	}
+	if err := stdInWriter.Close(); err != nil {
+		return "", err
+	}
+
+	sig, err := io.ReadAll(stdOutReader)
+	if err != nil {
+		return "", err
+	}
+
+	e, err := io.ReadAll(stdErrReader)
+	if err != nil {
+		return "", err
+	}
+
+	if len(e) > 0 {
+		fmt.Fprint(os.Stderr, string(e))
+	}
+
+	if err = cmd.Wait(); err != nil {
+		return "", err
+	}
+
+	if len(sig) == 0 {
+		return "", ErrUnableToSign
+	}
+
+	return string(sig), nil
+}
---
+++ internal/gitinterface/keys_test.go
@@ -0,0 +1,188 @@
+package gitinterface
+
+import (
+	"os"
+	"testing"
+
+	"github.com/adityasaky/gittuf/internal/common"
+	"github.com/go-git/go-git/v5/config"
+	format "github.com/go-git/go-git/v5/plumbing/format/config"
+)
+
+func TestGetSigningInfo(t *testing.T) {
+	testDir, err := common.CreateTestRepository()
+	if err != nil {
+		t.Error(err)
+	}
+	if err := os.Chdir(testDir); err != nil {
+		t.Error(err)
+	}
+
+	repo, err := common.GetRepositoryHandler()
+	if err != nil {
+		t.Error(err)
+	}
+
+	tests := map[string]struct {
+		c                   *config.Config
+		wantedSigningMethod SigningMethod
+		wantedKeyInfo       string
+		wantedProgram       string
+	}{
+		"gpg signing method, key abcdef": {
+			c: &config.Config{
+				Raw: &format.Config{
+					Sections: format.Sections{
+						&format.Section{
+							Name: "user",
+							Options: format.Options{
+								&format.Option{
+									Key:   "signingkey",
+									Value: "abcdef",
+								},
+							},
+						},
+					},
+				},
+			},
+			wantedSigningMethod: SigningMethodGPG,
+			wantedKeyInfo:       "abcdef",
+			wantedProgram:       "gpg",
+		},
+		"ssh signing method, key abcdef": {
+			c: &config.Config{
+				Raw: &format.Config{
+					Sections: format.Sections{
+						&format.Section{
+							Name: "user",
+							Options: format.Options{
+								&format.Option{
+									Key:   "signingkey",
+									Value: "abcdef",
+								},
+							},
+						},
+						&format.Section{
+							Name: "gpg",
+							Options: format.Options{
+								&format.Option{
+									Key:   "format",
+									Value: "ssh",
+								},
+							},
+						},
+					},
+				},
+			},
+			wantedSigningMethod: SigningMethodSSH,
+			wantedKeyInfo:       "abcdef",
+			wantedProgram:       "ssh-keygen",
+		},
+		"x509 signing method": {
+			c: &config.Config{
+				Raw: &format.Config{
+					Sections: format.Sections{
+						&format.Section{
+							Name: "user",
+							Options: format.Options{
+								&format.Option{
+									Key:   "signingkey",
+									Value: "abcdef",
+								},
+							},
+						},
+						&format.Section{
+							Name: "gpg",
+							Options: format.Options{
+								&format.Option{
+									Key:   "format",
+									Value: "x509",
+								},
+							},
+						},
+					},
+				},
+			},
+			wantedSigningMethod: SigningMethodX509,
+			wantedKeyInfo:       "abcdef",
+			wantedProgram:       "gpgsm",
+		},
+	}
+
+	for name, test := range tests {
+		if err := repo.SetConfig(test.c); err != nil {
+			t.Error(err)
+		}
+
+		signingMethod, keyInfo, program, err := getSigningInfo(repo)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if signingMethod != test.wantedSigningMethod {
+			t.Errorf("expected %d, got %d in test %s", test.wantedSigningMethod, signingMethod, name)
+		}
+		if keyInfo != test.wantedKeyInfo {
+			t.Errorf("expected %s, got %s in test %s", test.wantedKeyInfo, keyInfo, name)
+		}
+		if program != test.wantedProgram {
+			t.Errorf("expected %s, got %s in test %s", test.wantedProgram, program, name)
+		}
+	}
+
+	// FIXME: We have to mock the system / global configs, this test will fail
+	// because it'll detect the user's configuration.
+	// This config defaults to gpg, but no signing key is specified.
+	// if err := repo.SetConfig(&config.Config{
+	// 	Raw: &format.Config{
+	// 		Sections: format.Sections{
+	// 			&format.Section{
+	// 				Name: "user",
+	// 				Options: format.Options{
+	// 					&format.Option{
+	// 						Key:   "foo",
+	// 						Value: "bar",
+	// 					},
+	// 				},
+	// 			},
+	// 		},
+	// 	},
+	// }); err != nil {
+	// 	t.Error(err)
+	// }
+	// _, _, _, err = GetSigningInfo(repo)
+	// assert.ErrorIs(t, err, ErrSigningKeyNotSpecified)
+
+	// FIXME: We have to mock the system / global configs, this test will fail
+	// because it'll detect the user's configuration.
+	// // This config is set to use some unknown signing method.
+	// if err := repo.SetConfig(&config.Config{
+	// 	Raw: &format.Config{
+	// 		Sections: format.Sections{
+	// 			&format.Section{
+	// 				Name: "user",
+	// 				Options: format.Options{
+	// 					&format.Option{
+	// 						Key:   "signingkey",
+	// 						Value: "abcdef",
+	// 					},
+	// 				},
+	// 			},
+	// 			&format.Section{
+	// 				Name: "gpg",
+	// 				Options: format.Options{
+	// 					&format.Option{
+	// 						Key:   "format",
+	// 						Value: "abcdef",
+	// 					},
+	// 				},
+	// 			},
+	// 		},
+	// 	},
+	// }); err != nil {
+	// 	t.Error(err)
+	// }
+
+	// _, _, _, err = GetSigningInfo(repo)
+	// assert.ErrorIs(t, err, ErrUnknownSigningMethod)
+}
