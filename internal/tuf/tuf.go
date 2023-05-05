f03daa974985995986f8c95d125db5096d3cea3d
Aditya Sirish <aditya@saky.in>
2023-05-05 16:20:14 -0400

    tuf: Add metadata modification methods
    Signed-off-by: Aditya Sirish <aditya@saky.in>

--- internal/tuf/tuf.go
+++ internal/tuf/tuf.go
@@ -9,11 +9,11 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"errors"
-	"fmt"
-	"sync"
 
 	"github.com/secure-systems-lab/go-securesystemslib/cjson"
 )
+
+const specVersion = "1.0"
 
 var (
 	ErrTargetsNotEmpty = errors.New("`targets` field in gittuf Targets metadata must be empty")
@@ -26,7 +26,6 @@ type Key struct {
 	KeyVal              KeyVal   `json:"keyval"`
 	KeyIDHashAlgorithms []string `json:"keyid_hash_algorithms"`
 	keyID               string
-	idOnce              sync.Once
 }
 
 // KeyVal contains a `Public` field that records the public key value.
@@ -34,31 +33,51 @@ type KeyVal struct {
 	Public string `json:"public"`
 }
 
+// NewKey creates a new instance of Key for the values passed in.
+func NewKey(keyType string, scheme string, keyVal KeyVal, keyIDHashAlgorithms []string) (*Key, error) {
+	key := &Key{
+		KeyType:             keyType,
+		Scheme:              scheme,
+		KeyVal:              keyVal,
+		KeyIDHashAlgorithms: keyIDHashAlgorithms,
+	}
+	keyID, err := calculateKeyID(key)
+	key.keyID = keyID
+	return key, err
+}
+
 // LoadKeyFromBytes returns a pointer to a Key instance created from the
 // contents of the bytes. The key contents are expected to be in the custom
 // securesystemslib format.
 func LoadKeyFromBytes(contents []byte) (*Key, error) {
 	// FIXME: this assumes keys are stored in securesystemslib format.
-	var key Key
+	var key *Key
 	if err := json.Unmarshal(contents, &key); err != nil {
 		return nil, err
 	}
-	return &key, nil
+
+	keyID, err := calculateKeyID(key)
+	if err != nil {
+		return nil, err
+	}
+	key.keyID = keyID
+
+	return key, nil
 }
 
 // ID returns the key ID.
 func (k *Key) ID() string {
-	// Modified version of go-tuf's implementation to use a single Key ID.
-	k.idOnce.Do(func() {
-		data, err := cjson.EncodeCanonical(k)
-		if err != nil {
-			panic(fmt.Errorf("error creating key ID: %w", err))
-		}
-		digest := sha256.Sum256(data)
-		k.keyID = hex.EncodeToString(digest[:])
-	})
-
 	return k.keyID
+}
+
+func calculateKeyID(k *Key) (string, error) {
+	// Modified version of go-tuf's implementation to use a single Key ID.
+	canonical, err := cjson.EncodeCanonical(k)
+	if err != nil {
+		return "", err
+	}
+	digest := sha256.Sum256(canonical)
+	return hex.EncodeToString(digest[:]), nil
 }
 
 // Role records common characteristics recorded in a role entry in Root metadata
@@ -79,21 +98,104 @@ type RootMetadata struct {
 	Roles              map[string]Role `json:"roles"`
 }
 
+// NewRootMetadata returns a new instance of RootMetadata.
+func NewRootMetadata() *RootMetadata {
+	return &RootMetadata{
+		Type:               "root",
+		SpecVersion:        specVersion,
+		ConsistentSnapshot: true,
+	}
+}
+
+// SetVersion sets the version of the RootMetadata to the value passed in.
+func (r *RootMetadata) SetVersion(version int) {
+	r.Version = version
+}
+
+// SetExpires sets the expiry date of the RootMetadata to the value passed in.
+func (r *RootMetadata) SetExpires(expires string) {
+	r.Expires = expires
+}
+
+// AddKey adds a key to the RootMetadata instance.
+func (r *RootMetadata) AddKey(key Key) {
+	if r.Keys == nil {
+		r.Keys = map[string]Key{}
+	}
+
+	r.Keys[key.keyID] = key
+}
+
+// AddRole adds a role object and associates it with roleName in the
+// RootMetadata instance.
+func (r *RootMetadata) AddRole(roleName string, role Role) {
+	if r.Roles == nil {
+		r.Roles = map[string]Role{}
+	}
+
+	r.Roles[roleName] = role
+}
+
 // TargetsMetadata defines the schema of TUF's Targets role.
 type TargetsMetadata struct {
-	Type        string                 `json:"type"`
-	SpecVersion string                 `json:"spec_version"`
-	Version     int                    `json:"version"`
-	Expires     string                 `json:"expires"`
-	Targets     map[string]interface{} `json:"targets"`
-	Delegations *Delegations           `json:"delegations"`
+	Type        string         `json:"type"`
+	SpecVersion string         `json:"spec_version"`
+	Version     int            `json:"version"`
+	Expires     string         `json:"expires"`
+	Targets     map[string]any `json:"targets"`
+	Delegations *Delegations   `json:"delegations"`
+}
+
+// NewTargetsMetadata returns a new instance of TargetsMetadata.
+func NewTargetsMetadata() *TargetsMetadata {
+	return &TargetsMetadata{
+		Type:        "targets",
+		SpecVersion: specVersion,
+	}
+}
+
+// SetVersion sets the version of the TargetsMetadata to the value passed in.
+func (t *TargetsMetadata) SetVersion(version int) {
+	t.Version = version
+}
+
+// SetExpires sets the expiry date of the TargetsMetadata to the value passed
+// in.
+func (t *TargetsMetadata) SetExpires(expires string) {
+	t.Expires = expires
+}
+
+// Validate ensures the instance of TargetsMetadata matches gittuf expectations.
+func (t *TargetsMetadata) Validate() error {
+	if len(t.Targets) != 0 {
+		return ErrTargetsNotEmpty
+	}
+	return nil
 }
 
 // Delegations defines the schema for specifying delegations in TUF's Targets
 // metadata.
 type Delegations struct {
 	Keys  map[string]Key `json:"keys"`
-	Roles []*Delegation  `json:"roles"`
+	Roles []Delegation   `json:"roles"`
+}
+
+// AddKey adds a delegations key.
+func (d *Delegations) AddKey(key Key) {
+	if d.Keys == nil {
+		d.Keys = map[string]Key{}
+	}
+
+	d.Keys[key.keyID] = key
+}
+
+// AddDelegation adds a new delegation.
+func (d *Delegations) AddDelegation(delegation Delegation) {
+	if d.Roles == nil {
+		d.Roles = []Delegation{}
+	}
+
+	d.Roles = append(d.Roles, delegation)
 }
 
 // Delegation defines the schema for a single delegation entry. It differs from
@@ -105,12 +207,4 @@ type Delegation struct {
 	Terminating bool             `json:"terminating"`
 	Custom      *json.RawMessage `json:"custom,omitempty"`
 	Role
-}
-
-// Validate ensures the instance of TargetsMetadata matches gittuf expectations.
-func (t *TargetsMetadata) Validate() error {
-	if len(t.Targets) != 0 {
-		return ErrTargetsNotEmpty
-	}
-	return nil
 }
