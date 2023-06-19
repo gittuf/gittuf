0500452de11a019f8784d3566ef22735883ee6de
Aditya Sirish <aditya@saky.in>
2023-06-19 11:12:47 -0700

    repository: Add VerifyRef for user invocation
    Signed-off-by: Aditya Sirish <aditya@saky.in>

---
+++ internal/repository/verify.go
@@ -0,0 +1,20 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/adityasaky/gittuf/internal/policy"
+)
+
+func (r *Repository) VerifyRef(ctx context.Context, target string, full bool) error {
+	target, err := absoluteReference(r.r, target)
+	if err != nil {
+		return err
+	}
+
+	if full {
+		return policy.VerifyRefFull(ctx, r.r, target)
+	}
+
+	return policy.VerifyRef(ctx, r.r, target)
+}
