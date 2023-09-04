850447e7f36e39df8b707d2392bdc4afe4271039
Aditya Sirish <aditya@saky.in>
2023-09-04 11:54:18 -0400

    Remove unused helpers
    Signed-off-by: Aditya Sirish <aditya@saky.in>

--- internal/rsl/rsl_test.go
+++ internal/rsl/rsl_test.go
@@ -3,9 +3,7 @@ package rsl
 import (
 	"encoding/base64"
 	"fmt"
-	"io"
 	"os"
-	"path/filepath"
 	"testing"
 
 	"github.com/gittuf/gittuf/internal/gitinterface"
@@ -806,103 +804,5 @@ func TestParseRSLEntryMessage(t *testing.T) {
 				}
 			}
 		})
-	}
-}
-
-// copyDir recursively copies a directory tree, attempting to preserve permissions.
-// Source directory must exist, destination directory must *not* exist.
-// Symlinks are ignored and skipped.
-// Source: https://gist.github.com/r0l1/92462b38df26839a3ca324697c8cba04
-// Licensed MIT, Author: Roland Singer [roland.singer@desertbit.com]
-func copyDir(t *testing.T, src string, dst string) {
-	src = filepath.Clean(src)
-	dst = filepath.Clean(dst)
-
-	si, err := os.Stat(src)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if !si.IsDir() {
-		t.Fatal(fmt.Errorf("source is not a directory"))
-	}
-
-	_, err = os.Stat(dst)
-	if err != nil && !os.IsNotExist(err) {
-		t.Fatal(err)
-	}
-	if err == nil {
-		t.Fatal(fmt.Errorf("destination already exists"))
-	}
-
-	err = os.MkdirAll(dst, si.Mode())
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	entries, err := os.ReadDir(src)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	for _, entry := range entries {
-		srcPath := filepath.Join(src, entry.Name())
-		dstPath := filepath.Join(dst, entry.Name())
-
-		if entry.IsDir() {
-			copyDir(t, srcPath, dstPath)
-		} else {
-			// Skip symlinks.
-			if entry.Type()&os.ModeSymlink != 0 {
-				continue
-			}
-
-			copyFile(t, srcPath, dstPath)
-		}
-	}
-}
-
-// copyFile copies the contents of the file named src to the file named
-// by dst. The file will be created if it does not already exist. If the
-// destination file exists, all it's contents will be replaced by the contents
-// of the source file. The file mode will be copied from the source and
-// the copied data is synced/flushed to stable storage.
-// Source: https://gist.github.com/r0l1/92462b38df26839a3ca324697c8cba04
-// Licensed MIT, Author: Roland Singer [roland.singer@desertbit.com]
-func copyFile(t *testing.T, src, dst string) {
-	t.Helper()
-
-	in, err := os.Open(src)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer in.Close()
-
-	out, err := os.Create(dst)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer func() {
-		if e := out.Close(); e != nil {
-			t.Fatal(e)
-		}
-	}()
-
-	_, err = io.Copy(out, in)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	err = out.Sync()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	si, err := os.Stat(src)
-	if err != nil {
-		t.Fatal(err)
-	}
-	err = os.Chmod(dst, si.Mode())
-	if err != nil {
-		t.Fatal(err)
 	}
 }
