20db25a49ce6fcbe1abe87b6a84228b9d9c465ef
Aditya Sirish <aditya@saky.in>
2023-09-12 14:58:39 -0400

    cmd: Add rsl remote check command
    Signed-off-by: Aditya Sirish <aditya@saky.in>

---
+++ internal/cmd/rsl/remote/remote.go
@@ -0,0 +1,17 @@
+package remote
+
+import (
+	"github.com/gittuf/gittuf/internal/cmd/rsl/remote/check"
+	"github.com/spf13/cobra"
+)
+
+func New() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "remote",
+		Short: "Tools for managing remote RSLs",
+	}
+
+	cmd.AddCommand(check.New())
+
+	return cmd
+}
