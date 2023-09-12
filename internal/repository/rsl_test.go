044a0426b3123f1cac963e581a462e947e01871e
Aditya Sirish <aditya@saky.in>
2023-09-12 14:56:34 -0400

    repository: Add method to check for RSL updates
    Signed-off-by: Aditya Sirish <aditya@saky.in>

--- internal/repository/rsl_test.go
+++ internal/repository/rsl_test.go
@@ -1,9 +1,12 @@
 package repository
 
 import (
+	"context"
 	"fmt"
+	"os"
 	"testing"
 
+	"github.com/gittuf/gittuf/internal/gitinterface"
 	"github.com/gittuf/gittuf/internal/rsl"
 	"github.com/go-git/go-billy/v5/memfs"
 	"github.com/go-git/go-git/v5"
@@ -138,4 +141,211 @@ func TestRecordRSLAnnotation(t *testing.T) {
 	assert.Equal(t, "skip annotation", annotation.Message)
 	assert.Equal(t, []plumbing.Hash{entryID}, annotation.RSLEntryIDs)
 	assert.True(t, annotation.Skip)
+}
+
+func TestCheckRemoteRSLForUpdates(t *testing.T) {
+	remoteName := "origin"
+	refName := "refs/heads/main"
+	anotherRefName := "refs/heads/feature"
+
+	t.Run("remote has updates for local", func(t *testing.T) {
+		tmpDir, err := os.MkdirTemp("", "gittuf")
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer os.RemoveAll(tmpDir) //nolint:errcheck
+
+		// Simulate remote actions
+		remoteR, err := git.PlainInit(tmpDir, false)
+		if err != nil {
+			t.Fatal(err)
+		}
+		remoteRepo := &Repository{r: remoteR}
+
+		// We can't use remoteRepo.InitializeNamespaces() as it'll create zero
+		// namespace for policy, an issue when syncing.
+		if err := rsl.InitializeNamespace(remoteRepo.r); err != nil {
+			t.Fatal(err)
+		}
+
+		if _, err := gitinterface.Commit(remoteRepo.r, gitinterface.EmptyTree(), refName, "Test commit", false); err != nil {
+			t.Fatal(err)
+		}
+		if err := remoteRepo.RecordRSLEntryForReference(refName, false); err != nil {
+			t.Fatal(err)
+		}
+
+		// Clone remote repository
+		// TODO: this should be handled by the Repository package
+		localR, err := gitinterface.CloneAndFetchToMemory(context.Background(), tmpDir, refName, []string{rsl.RSLRef}, false)
+		if err != nil {
+			t.Fatal(err)
+		}
+		localRepo := &Repository{r: localR}
+
+		// Simulate more remote actions
+		if _, err := gitinterface.Commit(remoteRepo.r, gitinterface.EmptyTree(), refName, "Test commit", false); err != nil {
+			t.Fatal(err)
+		}
+		if err := remoteRepo.RecordRSLEntryForReference(refName, false); err != nil {
+			t.Fatal(err)
+		}
+
+		// Local should be notified that remote has updates
+		hasUpdates, hasDiverged, err := localRepo.CheckRemoteRSLForUpdates(context.Background(), remoteName)
+		assert.Nil(t, err)
+		assert.True(t, hasUpdates)
+		assert.False(t, hasDiverged)
+	})
+
+	t.Run("remote has no updates for local", func(t *testing.T) {
+		tmpDir, err := os.MkdirTemp("", "gittuf")
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer os.RemoveAll(tmpDir) //nolint:errcheck
+
+		// Simulate remote actions
+		remoteR, err := git.PlainInit(tmpDir, false)
+		if err != nil {
+			t.Fatal(err)
+		}
+		remoteRepo := &Repository{r: remoteR}
+
+		// We can't use remoteRepo.InitializeNamespaces() as it'll create zero
+		// namespace for policy, an issue when syncing.
+		if err := rsl.InitializeNamespace(remoteRepo.r); err != nil {
+			t.Fatal(err)
+		}
+
+		if _, err := gitinterface.Commit(remoteRepo.r, gitinterface.EmptyTree(), refName, "Test commit", false); err != nil {
+			t.Fatal(err)
+		}
+		if err := remoteRepo.RecordRSLEntryForReference(refName, false); err != nil {
+			t.Fatal(err)
+		}
+
+		// Clone remote repository
+		// TODO: this should be handled by the Repository package
+		localR, err := gitinterface.CloneAndFetchToMemory(context.Background(), tmpDir, refName, []string{rsl.RSLRef}, false)
+		if err != nil {
+			t.Fatal(err)
+		}
+		localRepo := &Repository{r: localR}
+
+		// Local should be notified that remote has no updates
+		hasUpdates, hasDiverged, err := localRepo.CheckRemoteRSLForUpdates(context.Background(), remoteName)
+		assert.Nil(t, err)
+		assert.False(t, hasUpdates)
+		assert.False(t, hasDiverged)
+	})
+
+	t.Run("local is ahead of remote", func(t *testing.T) {
+		tmpDir, err := os.MkdirTemp("", "gittuf")
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer os.RemoveAll(tmpDir) //nolint:errcheck
+
+		// Simulate remote actions
+		remoteR, err := git.PlainInit(tmpDir, false)
+		if err != nil {
+			t.Fatal(err)
+		}
+		remoteRepo := &Repository{r: remoteR}
+
+		// We can't use remoteRepo.InitializeNamespaces() as it'll create zero
+		// namespace for policy, an issue when syncing.
+		if err := rsl.InitializeNamespace(remoteRepo.r); err != nil {
+			t.Fatal(err)
+		}
+
+		if _, err := gitinterface.Commit(remoteRepo.r, gitinterface.EmptyTree(), refName, "Test commit", false); err != nil {
+			t.Fatal(err)
+		}
+		if err := remoteRepo.RecordRSLEntryForReference(refName, false); err != nil {
+			t.Fatal(err)
+		}
+
+		// Clone remote repository
+		// TODO: this should be handled by the Repository package
+		localR, err := gitinterface.CloneAndFetchToMemory(context.Background(), tmpDir, refName, []string{rsl.RSLRef}, false)
+		if err != nil {
+			t.Fatal(err)
+		}
+		localRepo := &Repository{r: localR}
+
+		// Simulate local actions
+		if _, err := gitinterface.Commit(localRepo.r, gitinterface.EmptyTree(), refName, "Test commit", false); err != nil {
+			t.Fatal(err)
+		}
+		if err := localRepo.RecordRSLEntryForReference(refName, false); err != nil {
+			t.Fatal(err)
+		}
+
+		// Local should be notified that remote has no updates
+		hasUpdates, hasDiverged, err := localRepo.CheckRemoteRSLForUpdates(context.Background(), remoteName)
+		assert.Nil(t, err)
+		assert.False(t, hasUpdates)
+		assert.False(t, hasDiverged)
+	})
+
+	t.Run("remote and local have diverged", func(t *testing.T) {
+		tmpDir, err := os.MkdirTemp("", "gittuf")
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer os.RemoveAll(tmpDir) //nolint:errcheck
+
+		// Simulate remote actions
+		remoteR, err := git.PlainInit(tmpDir, false)
+		if err != nil {
+			t.Fatal(err)
+		}
+		remoteRepo := &Repository{r: remoteR}
+
+		// We can't use remoteRepo.InitializeNamespaces() as it'll create zero
+		// namespace for policy, an issue when syncing.
+		if err := rsl.InitializeNamespace(remoteRepo.r); err != nil {
+			t.Fatal(err)
+		}
+
+		if _, err := gitinterface.Commit(remoteRepo.r, gitinterface.EmptyTree(), refName, "Test commit", false); err != nil {
+			t.Fatal(err)
+		}
+		if err := remoteRepo.RecordRSLEntryForReference(refName, false); err != nil {
+			t.Fatal(err)
+		}
+
+		// Clone remote repository
+		// TODO: this should be handled by the Repository package
+		localR, err := gitinterface.CloneAndFetchToMemory(context.Background(), tmpDir, refName, []string{rsl.RSLRef}, false)
+		if err != nil {
+			t.Fatal(err)
+		}
+		localRepo := &Repository{r: localR}
+
+		// Simulate remote actions
+		if _, err := gitinterface.Commit(remoteRepo.r, gitinterface.EmptyTree(), refName, "Test commit", false); err != nil {
+			t.Fatal(err)
+		}
+		if err := remoteRepo.RecordRSLEntryForReference(refName, false); err != nil {
+			t.Fatal(err)
+		}
+
+		// Simulate local actions
+		if _, err := gitinterface.Commit(localRepo.r, gitinterface.EmptyTree(), anotherRefName, "Test commit", false); err != nil {
+			t.Fatal(err)
+		}
+		if err := localRepo.RecordRSLEntryForReference(anotherRefName, false); err != nil {
+			t.Fatal(err)
+		}
+
+		// Local should be notified that remote has updates that needs to be
+		// reconciled
+		hasUpdates, hasDiverged, err := localRepo.CheckRemoteRSLForUpdates(context.Background(), remoteName)
+		assert.Nil(t, err)
+		assert.True(t, hasUpdates)
+		assert.True(t, hasDiverged)
+	})
 }
