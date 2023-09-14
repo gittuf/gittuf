5431e25aa72b3fe91c6d060834585e816cc093a1
Aditya Sirish <aditya@saky.in>
2023-09-14 12:22:51 -0400

    cmd: Add clone
    Signed-off-by: Aditya Sirish <aditya@saky.in>

---
+++ internal/cmd/clone/clone.go
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package clone
+
+import (
+	"github.com/gittuf/gittuf/internal/repository"
+	"github.com/spf13/cobra"
+)
+
+type options struct {
+	branch string
+}
+
+func (o *options) AddFlags(cmd *cobra.Command) {
+	cmd.Flags().StringVarP(
+		&o.branch,
+		"branch",
+		"b",
+		"",
+		"Specify branch to check out",
+	)
+}
+
+func (o *options) Run(cmd *cobra.Command, args []string) error {
+	var dir string
+	if len(args) > 1 {
+		dir = args[1]
+	}
+	_, err := repository.Clone(cmd.Context(), args[0], dir, o.branch)
+	return err
+}
+
+func New() *cobra.Command {
+	o := &options{}
+	cmd := &cobra.Command{
+		Use:   "clone",
+		Short: "Clone repository and its gittuf references",
+		Args:  cobra.MinimumNArgs(1),
+		RunE:  o.Run,
+	}
+	o.AddFlags(cmd)
+
+	return cmd
+}
--- internal/cmd/root/root.go
+++ internal/cmd/root/root.go
@@ -3,6 +3,7 @@
 package root
 
 import (
+	"github.com/gittuf/gittuf/internal/cmd/clone"
 	"github.com/gittuf/gittuf/internal/cmd/policy"
 	"github.com/gittuf/gittuf/internal/cmd/rsl"
 	"github.com/gittuf/gittuf/internal/cmd/trust"
@@ -18,6 +19,7 @@ func New() *cobra.Command {
 		Short: "A security layer for Git repositories, powered by TUF",
 	}
 
+	cmd.AddCommand(clone.New())
 	cmd.AddCommand(trust.New())
 	cmd.AddCommand(policy.New())
 	cmd.AddCommand(rsl.New())
