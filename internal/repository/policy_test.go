a20a435f8d07bb89f7774c39aea7d522b0903a82
Aditya Sirish <aditya@saky.in>
2023-10-12 11:07:57 -0400

    repository: Add PullPolicy
    Signed-off-by: Aditya Sirish <aditya@saky.in>

--- internal/repository/policy_test.go
+++ internal/repository/policy_test.go
@@ -8,9 +8,11 @@ import (
 
 	"github.com/gittuf/gittuf/internal/policy"
 	"github.com/gittuf/gittuf/internal/rsl"
+	"github.com/go-git/go-billy/v5/memfs"
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/config"
 	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/storage/memory"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -71,4 +73,66 @@ func TestPushPolicy(t *testing.T) {
 		err = localRepo.PushPolicy(context.Background(), remoteName)
 		assert.ErrorIs(t, err, ErrPushingPolicy)
 	})
+}
+
+func TestPullPolicy(t *testing.T) {
+	remoteName := "origin"
+
+	t.Run("successful pull", func(t *testing.T) {
+		remoteTmpDir := t.TempDir()
+		remoteRepo := createTestRepositoryWithPolicy(t, remoteTmpDir)
+
+		localRepoR, err := git.Init(memory.NewStorage(), memfs.New())
+		if err != nil {
+			t.Fatal(err)
+		}
+		localRepo := &Repository{r: localRepoR}
+		if _, err := localRepo.r.CreateRemote(&config.RemoteConfig{
+			Name: remoteName,
+			URLs: []string{remoteTmpDir},
+		}); err != nil {
+			t.Fatal(err)
+		}
+
+		err = localRepo.PullPolicy(context.Background(), remoteName)
+		assert.Nil(t, err)
+
+		assertLocalAndRemoteRefsMatch(t, localRepo.r, remoteRepo.r, policy.PolicyRef)
+		assertLocalAndRemoteRefsMatch(t, localRepo.r, remoteRepo.r, rsl.Ref)
+
+		// No updates, successful push
+		err = localRepo.PullPolicy(context.Background(), remoteName)
+		assert.Nil(t, err)
+	})
+
+	// t.Run("divergent policies, unsuccessful pull", func(t *testing.T) {
+	// 	remoteTmpDir := t.TempDir()
+	// 	createTestRepositoryWithPolicy(t, remoteTmpDir)
+
+	// 	localRepoR, err := git.Init(memory.NewStorage(), memfs.New())
+	// 	if err != nil {
+	// 		t.Fatal(err)
+	// 	}
+	// 	localRepo := &Repository{r: localRepoR}
+
+	// 	if err := rsl.InitializeNamespace(localRepo.r); err != nil {
+	// 		t.Fatal(err)
+	// 	}
+
+	// 	if err := rsl.NewEntry(policy.PolicyRef, plumbing.ZeroHash).Commit(localRepo.r, false); err != nil {
+	// 		t.Fatal(err)
+	// 	}
+
+	// 	if _, err := localRepo.r.CreateRemote(&config.RemoteConfig{
+	// 		Name: remoteName,
+	// 		URLs: []string{remoteTmpDir},
+	// 	}); err != nil {
+	// 		t.Fatal(err)
+	// 	}
+
+	// 	err = localRepo.PullPolicy(context.Background(), remoteName)
+	// 	assert.ErrorIs(t, err, ErrPullingPolicy)
+	// })
+	// FIXME: this may be a bug in how fast-forward only fetches are handled in
+	// go-git. See: https://github.com/go-git/go-git/issues/874
 }
