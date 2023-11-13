85d5f5195a892c0be6235903625532d90dedd653
spectre10 <shyamthakkar001@gmail.com>
2023-11-14 02:56:55 +0530

    fix: signing-key required for remote subcommands.
    Signed-off-by: spectre10 <shyamthakkar001@gmail.com>

--- internal/cmd/policy/policy.go
+++ internal/cmd/policy/policy.go
@@ -23,8 +23,12 @@ func New() *cobra.Command {
 	cmd.AddCommand(i.New(o))
 	cmd.AddCommand(addkey.New(o))
 	cmd.AddCommand(addrule.New(o))
-	cmd.AddCommand(remote.New())
 	cmd.AddCommand(removerule.New(o))
+
+	remoteCmd := remote.New()
+	cmd.AddCommand(remoteCmd)
+	// set signing-key as not required for remote command
+	remoteCmd.InheritedFlags().SetAnnotation("signing-key", cobra.BashCompOneRequiredFlag, []string{"false"}) // nolint:errcheck
 
 	return cmd
 }
