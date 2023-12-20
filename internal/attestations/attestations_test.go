4da426a4b975e730dfd675e45b10b3ce61009566
Aditya Sirish <aditya@saky.in>
2023-12-19 21:48:02 -0500

    attestations: Fix go-git import
    Signed-off-by: Aditya Sirish <aditya@saky.in>

--- internal/attestations/attestations_test.go
+++ internal/attestations/attestations_test.go
@@ -9,10 +9,10 @@ import (
 	"github.com/gittuf/gittuf/internal/gitinterface"
 	"github.com/gittuf/gittuf/internal/rsl"
 	"github.com/gittuf/gittuf/internal/signerverifier/dsse"
-	"github.com/gittuf/gittuf/internal/third_party/go-git"
-	"github.com/gittuf/gittuf/internal/third_party/go-git/plumbing"
-	"github.com/gittuf/gittuf/internal/third_party/go-git/storage/memory"
 	"github.com/go-git/go-billy/v5/memfs"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/storage/memory"
 	"github.com/stretchr/testify/assert"
 )
 
--- internal/attestations/authorization_test.go
+++ internal/attestations/authorization_test.go
@@ -8,10 +8,10 @@ import (
 	_ "embed"
 
 	"github.com/gittuf/gittuf/internal/signerverifier/dsse"
-	"github.com/gittuf/gittuf/internal/third_party/go-git"
-	"github.com/gittuf/gittuf/internal/third_party/go-git/plumbing"
-	"github.com/gittuf/gittuf/internal/third_party/go-git/storage/memory"
 	"github.com/go-git/go-billy/v5/memfs"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/storage/memory"
 	ita "github.com/in-toto/attestation/go/v1"
 	sslibdsse "github.com/secure-systems-lab/go-securesystemslib/dsse"
 	"github.com/stretchr/testify/assert"
