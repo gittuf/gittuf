c6df61360dc3e8bae90f855102e2a8d6ec246b4e
Aditya Sirish <aditya@saky.in>
2024-01-15 13:38:13 -0500

    cmd: Add gittuf policy sign
    Signed-off-by: Aditya Sirish <aditya@saky.in>

---
+++ internal/cmd/policy/sign/sign.go
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package sign
+
+import (
+	"os"
+
+	"github.com/gittuf/gittuf/internal/cmd/common"
+	"github.com/gittuf/gittuf/internal/cmd/policy/persistent"
+	"github.com/gittuf/gittuf/internal/policy"
+	"github.com/gittuf/gittuf/internal/repository"
+	"github.com/spf13/cobra"
+)
+
+type options struct {
+	p          *persistent.Options
+	policyName string
+}
+
+func (o *options) AddFlags(cmd *cobra.Command) {
+	cmd.Flags().StringVar(
+		&o.policyName,
+		"policy-name",
+		policy.TargetsRoleName,
+		"policy file to sign",
+	)
+}
+
+func (o *options) Run(cmd *cobra.Command, _ []string) error {
+	repo, err := repository.LoadRepository()
+	if err != nil {
+		return err
+	}
+
+	keyBytes, err := os.ReadFile(o.p.SigningKey)
+	if err != nil {
+		return err
+	}
+	signer, err := common.LoadSigner(keyBytes)
+	if err != nil {
+		return err
+	}
+
+	return repo.SignTargets(cmd.Context(), signer, o.policyName, true)
+}
+
+func New(persistent *persistent.Options) *cobra.Command {
+	o := &options{p: persistent}
+	cmd := &cobra.Command{
+		Use:               "sign",
+		Short:             "Sign policy file",
+		Long:              "This command allows users to add their signature to the specified policy file.",
+		PreRunE:           common.CheckIfSigningViable,
+		RunE:              o.Run,
+		DisableAutoGenTag: true,
+	}
+	o.AddFlags(cmd)
+
+	return cmd
+}
