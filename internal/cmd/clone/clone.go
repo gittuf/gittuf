62a741d5ad26d957d88dac80a418475e7acb1c1e
neilnaveen <42328488+neilnaveen@users.noreply.github.com>
2024-01-19 12:35:31 -0600

    Add support to check if expected root keys are the actual root keys in clone.go
    - Added the expectedRootKeys flag to clone.go
    - Added addtional test in sync_test.go for expectedRootKeys
    
    Signed-off-by: neilnaveen <42328488+neilnaveen@users.noreply.github.com>

--- internal/cmd/clone/clone.go
+++ internal/cmd/clone/clone.go
@@ -3,12 +3,16 @@
 package clone
 
 import (
+	"os"
+
 	"github.com/gittuf/gittuf/internal/repository"
+	"github.com/gittuf/gittuf/internal/tuf"
 	"github.com/spf13/cobra"
 )
 
 type options struct {
-	branch string
+	branch           string
+	expectedRootKeys []string
 }
 
 func (o *options) AddFlags(cmd *cobra.Command) {
@@ -19,6 +23,13 @@ func (o *options) AddFlags(cmd *cobra.Command) {
 		"",
 		"specify branch to check out",
 	)
+	cmd.Flags().StringSliceVarP(
+		&o.expectedRootKeys,
+		"expectedRootKeys",
+		"e",
+		[]string{},
+		"specify expected root keys in cloned repo",
+	)
 }
 
 func (o *options) Run(cmd *cobra.Command, args []string) error {
@@ -26,7 +37,23 @@ func (o *options) Run(cmd *cobra.Command, args []string) error {
 	if len(args) > 1 {
 		dir = args[1]
 	}
-	_, err := repository.Clone(cmd.Context(), args[0], dir, o.branch)
+	expectedRootKeys := []*tuf.Key{}
+
+	for _, keyPath := range o.expectedRootKeys {
+		keyBytes, err := os.ReadFile(keyPath)
+		if err != nil {
+			return err
+		}
+
+		key, err := tuf.LoadKeyFromBytes(keyBytes)
+		if err != nil {
+			return err
+		}
+
+		expectedRootKeys = append(expectedRootKeys, key)
+	}
+
+	_, err := repository.Clone(cmd.Context(), args[0], dir, o.branch, expectedRootKeys)
 	return err
 }
 
