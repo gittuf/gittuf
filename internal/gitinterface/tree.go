5498ffb46636ae379f5adaa27e74dd7a72e94f1f
Aditya Sirish <aditya@saky.in>
2024-05-07 09:57:01 -0400

    gitinterface: Add support for trees
    Signed-off-by: Aditya Sirish <aditya@saky.in>

--- internal/gitinterface/tree.go
+++ internal/gitinterface/tree.go
@@ -4,6 +4,7 @@ package gitinterface
 
 import (
 	"errors"
+	"fmt"
 	"io"
 	"os/exec"
 	"path"
@@ -50,6 +51,20 @@ func EmptyTree() plumbing.Hash {
 	return obj.Hash()
 }
 
+func (r *Repository) EmptyTree() (Hash, error) {
+	stdOut, stdErr, err := r.executeGitCommandWithStdIn(nil, "hash-object", "-t", "tree", "--stdin")
+	if err != nil {
+		return ZeroHash, fmt.Errorf("unable to hash empty tree: %s", stdErr)
+	}
+
+	hash, err := NewHash(strings.TrimSpace(stdOut))
+	if err != nil {
+		return ZeroHash, fmt.Errorf("empty tree has invalid Git ID: %w", err)
+	}
+
+	return hash, nil
+}
+
 // GetAllFilesInTree returns all filepaths and the corresponding hash in the
 // specified tree.
 func GetAllFilesInTree(tree *object.Tree) (map[string]plumbing.Hash, error) {
@@ -74,6 +89,39 @@ func GetAllFilesInTree(tree *object.Tree) (map[string]plumbing.Hash, error) {
 		}
 
 		files[name] = entry.Hash
+	}
+
+	return files, nil
+}
+
+func (r *Repository) GetAllFilesInTree(treeID Hash) (map[string]Hash, error) {
+	stdOut, stdErr, err := r.executeGitCommand("ls-tree", "-r", "--format=%(path) %(objectname)", treeID.String())
+	if err != nil {
+		return nil, fmt.Errorf("unable to enumerate all files in tree: %s", stdErr)
+	}
+	stdOut = strings.TrimSpace(stdOut)
+
+	if stdOut == "" {
+		return nil, nil // alternatively, just check if treeID is empty tree?
+	}
+
+	entries := strings.Split(stdOut, "\n")
+	if len(entries) == 0 {
+		return nil, nil
+	}
+
+	files := map[string]Hash{}
+	for _, entry := range entries {
+		// we control entry's format in --format above, so no need to check
+		// length of split
+		entrySplit := strings.Split(entry, " ")
+
+		hash, err := NewHash(entrySplit[1])
+		if err != nil {
+			return nil, fmt.Errorf("invalid Git ID '%s' for path '%s': %w", entrySplit[1], entrySplit[0], err)
+		}
+
+		files[entrySplit[0]] = hash
 	}
 
 	return files, nil
@@ -109,6 +157,31 @@ func GetMergeTree(repo *git.Repository, commitAID, commitBID string) (string, er
 
 	stdOutString := strings.TrimSpace(string(stdOut))
 	return stdOutString, nil
+}
+
+func (r *Repository) GetMergeTree(commitAID, commitBID Hash) (Hash, error) {
+	if err := r.ensureIsCommit(commitAID); err != nil {
+		return ZeroHash, err
+	}
+	if err := r.ensureIsCommit(commitBID); err != nil {
+		return ZeroHash, err
+	}
+
+	if commitAID.IsZero() {
+		return r.GetCommitTreeID(commitBID)
+	}
+
+	stdOut, stdErr, err := r.executeGitCommand("merge-tree", commitAID.String(), commitBID.String())
+	if err != nil {
+		return ZeroHash, fmt.Errorf("unable to compute merge tree: %s", stdErr)
+	}
+
+	treeHash, err := NewHash(strings.TrimSpace(stdOut))
+	if err != nil {
+		return ZeroHash, fmt.Errorf("invalid merge tree ID: %w", err)
+	}
+
+	return treeHash, nil
 }
 
 // TreeBuilder is used to create multi-level trees in a repository.
@@ -196,4 +269,113 @@ func (t *TreeBuilder) writeTrees(parent string, tree *object.Tree) (plumbing.Has
 	}
 
 	return WriteTree(t.r, tree.Entries)
+}
+
+type ReplacementTreeBuilder struct {
+	repo    *Repository
+	trees   map[string]*entry
+	entries map[string]*entry
+}
+
+func NewReplacementTreeBuilder(repo *Repository) *ReplacementTreeBuilder {
+	return &ReplacementTreeBuilder{repo: repo}
+}
+
+func (t *ReplacementTreeBuilder) WriteRootTreeFromBlobIDs(files map[string]Hash) (Hash, error) {
+	rootNoteKey := ""
+	t.trees = map[string]*entry{rootNoteKey: {}}
+	t.entries = map[string]*entry{}
+
+	for path, gitID := range files {
+		t.buildIntermediates(path, gitID)
+	}
+
+	return t.writeTrees(rootNoteKey, t.trees[rootNoteKey])
+}
+
+func (t *ReplacementTreeBuilder) buildIntermediates(name string, gitID Hash) {
+	parts := strings.Split(name, "/")
+
+	var fullPath string
+	for _, part := range parts {
+		parent := fullPath
+		fullPath = path.Join(fullPath, part)
+
+		t.buildTree(name, parent, fullPath, gitID)
+	}
+}
+
+func (t *ReplacementTreeBuilder) buildTree(name, parent, fullPath string, gitID Hash) {
+	if _, ok := t.trees[fullPath]; ok {
+		return
+	}
+
+	if _, ok := t.entries[fullPath]; ok {
+		return
+	}
+
+	entryObj := &entry{name: path.Base(fullPath), gitID: ZeroHash}
+
+	if fullPath == name {
+		entryObj.isDir = false
+		entryObj.gitID = gitID
+	} else {
+		entryObj.isDir = true
+		t.trees[fullPath] = &entry{}
+	}
+
+	t.trees[parent].entries = append(t.trees[parent].entries, entryObj)
+}
+
+func (t *ReplacementTreeBuilder) writeTrees(parent string, tree *entry) (Hash, error) {
+	for i, e := range tree.entries {
+		if !e.isDir && !e.gitID.IsZero() {
+			continue
+		}
+
+		p := path.Join(parent, e.name)
+		entryID, err := t.writeTrees(p, t.trees[p])
+		if err != nil {
+			return ZeroHash, err
+		}
+		e.gitID = entryID
+
+		tree.entries[i] = e
+	}
+
+	return t.writeTree(tree.entries)
+}
+
+func (t *ReplacementTreeBuilder) writeTree(entries []*entry) (Hash, error) {
+	input := ""
+	for _, entry := range entries {
+		// this is very opinionated about the modes right now because the plan
+		// is to use it for gittuf metadata, which requires regular files and
+		// subdirectories
+		if entry.isDir {
+			input += "040000 tree " + entry.gitID.String() + "\t" + entry.name
+		} else {
+			input += "100644 blob " + entry.gitID.String() + "\t" + entry.name
+		}
+		input += "\n"
+	}
+
+	stdOut, stdErr, err := t.repo.executeGitCommandWithStdIn([]byte(input), "mktree")
+	if err != nil {
+		return ZeroHash, fmt.Errorf("unable to write Git tree: %s", stdErr)
+	}
+
+	treeID, err := NewHash(strings.TrimSpace(stdOut))
+	if err != nil {
+		return ZeroHash, fmt.Errorf("invalid tree ID: %w", err)
+	}
+
+	return treeID, nil
+}
+
+type entry struct {
+	name    string
+	isDir   bool
+	gitID   Hash
+	entries []*entry // only used when isDir is true
 }
