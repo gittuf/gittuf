fb5186d54fe40753dd575d09354f98593f734ef6
Aditya Sirish <aditya@saky.in>
2024-05-07 10:01:15 -0400

    gitinterface: Add change and config management
    Signed-off-by: Aditya Sirish <aditya@saky.in>

--- internal/gitinterface/log.go
+++ internal/gitinterface/log.go
@@ -4,7 +4,9 @@ package gitinterface
 
 import (
 	"errors"
+	"fmt"
 	"sort"
+	"strings"
 
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
@@ -75,4 +77,47 @@ func GetCommitsBetweenRange(repo *git.Repository, commitNewID, commitOldID plumb
 	}
 
 	return commits, nil
+}
+
+// GetCommitsBetweenRange returns the IDs of the commits that exist between the
+// specified new and old commit identifiers.
+func (r *Repository) GetCommitsBetweenRange(commitNewID, commitOldID Hash) ([]Hash, error) {
+	var args []string
+	if commitOldID.IsZero() {
+		args = []string{"rev-list", commitNewID.String()}
+	} else {
+		args = []string{"rev-list", fmt.Sprintf("%s..%s", commitOldID.String(), commitNewID.String())}
+	}
+
+	commitRangeString, err := r.executeGitCommandString(args...)
+	if err != nil {
+		return nil, fmt.Errorf("unable to enumerate commits in range: %w", err)
+	}
+
+	commitRangeSplit := strings.Split(commitRangeString, "\n")
+
+	commitRange := make([]Hash, 0, len(commitRangeSplit))
+	for _, cID := range commitRangeSplit {
+		if cID == "" {
+			continue
+		}
+		hash, err := NewHash(cID)
+		if err != nil {
+			return nil, err
+		}
+		commitRange = append(commitRange, hash)
+	}
+
+	// FIXME: we should ideally be sorting this in the order of occurrence
+	// rather than by commit ID. The only reason this is happening is because
+	// the ordering of commitRange by default is not deterministic. Rather than
+	// walking through them and identifying the right order, we're sorting by
+	// commit ID. The intended use case of this function is to get a list of
+	// commits that are then checked for the changes they introduce. At that
+	// point, they must be diffed with their parent directly.
+	sort.Slice(commitRange, func(i, j int) bool {
+		return commitRange[i].String() < commitRange[j].String()
+	})
+
+	return commitRange, nil
 }
