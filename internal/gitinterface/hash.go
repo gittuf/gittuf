c92dbeaa4f34c7f8e587c14f4cbd6900601c8394
Aditya Sirish <aditya@saky.in>
2024-05-07 09:53:24 -0400

    gitinterface: Add representation of a repository
    This commit introduces gittuf's custom representation of a Git
    repository. In addition, it introduces notion of a Hash type to
    represent a Git ID. This can be either SHA-1 or SHA-256.
    
    Signed-off-by: Aditya Sirish <aditya@saky.in>

---
+++ internal/gitinterface/hash.go
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package gitinterface
+
+import (
+	"bytes"
+	"crypto/sha1" //nolint:gosec
+	"crypto/sha256"
+	"encoding/hex"
+	"errors"
+)
+
+var (
+	zeroSHA1HashBytes   = [sha1.Size]byte{}
+	zeroSHA256HashBytes = [sha256.Size]byte{}
+)
+
+var (
+	ErrInvalidHashEncoding = errors.New("hash string is not hex encoded")
+	ErrInvalidHashLength   = errors.New("hash string is wrong length")
+)
+
+// Hash represents a Git object hash. It is a lightweight wrapper around the
+// standard hex encoded representation of a SHA-1 or SHA-256 hash used by Git.
+
+type Hash []byte
+
+// String returns the hex encoded hash.
+func (h Hash) String() string {
+	return hex.EncodeToString(h)
+}
+
+// IsZero compares the hash to see if it's the zero hash for either SHA-1 or
+// SHA-256.
+func (h Hash) IsZero() bool {
+	return bytes.Equal(h, zeroSHA1HashBytes[:]) || bytes.Equal(h, zeroSHA256HashBytes[:])
+}
+
+// ZeroHash represents an empty Hash.
+// TODO: use SHA-256 zero hash for repositories that have that as the default.
+var ZeroHash = Hash(zeroSHA1HashBytes[:])
+
+// NewHash returns a Hash object after ensuring the input string is correctly
+// encoded.
+func NewHash(h string) (Hash, error) {
+	if len(h) != (sha1.Size*2) && len(h) != (sha256.Size*2) {
+		return ZeroHash, ErrInvalidHashLength
+	}
+
+	hash, err := hex.DecodeString(h)
+	if err != nil {
+		return ZeroHash, ErrInvalidHashEncoding
+	}
+
+	return hash, nil
+}
