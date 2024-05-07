407f479cad120d80eb69be490a7349d2723c98f5
neilnaveen <42328488+neilnaveen@users.noreply.github.com>
2024-05-07 11:07:23 -0500

    Changed the output of list-rules to also incorporate changes in policy-staging
    Signed-off-by: neilnaveen <42328488+neilnaveen@users.noreply.github.com>

--- internal/cmd/policy/listrules/listrules.go
+++ internal/cmd/policy/listrules/listrules.go
@@ -6,22 +6,16 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/gittuf/gittuf/internal/policy"
 	"github.com/gittuf/gittuf/internal/repository"
+	"github.com/gittuf/gittuf/internal/tuf"
+	"github.com/sergi/go-diff/diffmatchpatch"
 	"github.com/spf13/cobra"
 )
 
-type options struct {
-	targetRef string
-}
+type options struct{}
 
-func (o *options) AddFlags(cmd *cobra.Command) {
-	cmd.Flags().StringVar(
-		&o.targetRef,
-		"target-ref",
-		"policy",
-		"specify which policy ref should be inspected",
-	)
-}
+func (o *options) AddFlags(_ *cobra.Command) {}
 
 func (o *options) Run(cmd *cobra.Command, _ []string) error {
 	repo, err := repository.LoadRepository()
@@ -29,45 +23,110 @@ func (o *options) Run(cmd *cobra.Command, _ []string) error {
 		return err
 	}
 
-	rules, err := repo.ListRules(cmd.Context(), o.targetRef)
+	policyRules, err := repo.ListRules(cmd.Context(), policy.PolicyRef)
 	if err != nil {
 		return err
 	}
 
-	// Iterate through the rules, they are already in order, and the depth tells us how to indent.
-	// The order is a pre-order traversal of the delegation tree, so that the parent is always before the children.
-
-	for _, curRule := range rules {
-		fmt.Printf(strings.Repeat("    ", curRule.Depth)+"Rule %s:\n", curRule.Delegation.Name)
-		gitpaths, filepaths := []string{}, []string{}
-		for _, path := range curRule.Delegation.Paths {
-			if strings.HasPrefix(path, "git:") {
-				gitpaths = append(gitpaths, path)
-			} else {
-				filepaths = append(filepaths, path)
-			}
-		}
-		if len(filepaths) > 0 {
-			fmt.Println(strings.Repeat("    ", curRule.Depth+1) + "Paths affected:")
-			for _, v := range filepaths {
-				fmt.Printf(strings.Repeat("    ", curRule.Depth+2)+"%s\n", v)
-			}
-		}
-		if len(gitpaths) > 0 {
-			fmt.Println(strings.Repeat("    ", curRule.Depth+1) + "Refs affected:")
-			for _, v := range gitpaths {
-				fmt.Printf(strings.Repeat("    ", curRule.Depth+2)+"%s\n", v)
-			}
-		}
-
-		fmt.Println(strings.Repeat("    ", curRule.Depth+1) + "Authorized keys:")
-		for _, key := range curRule.Delegation.Role.KeyIDs {
-			fmt.Printf(strings.Repeat("    ", curRule.Depth+2)+"%s\n", key)
-		}
-
-		fmt.Println(strings.Repeat("    ", curRule.Depth+1) + fmt.Sprintf("Required valid signatures: %d", curRule.Delegation.Role.Threshold))
+	policyStagingRules, err := repo.ListRules(cmd.Context(), policy.PolicyStagingRef)
+	if err != nil {
+		return err
 	}
+
+	policyPointer, policyStagingPointer := 0, 0
+
+	var fullDiff strings.Builder
+
+	fullDiff.WriteString("Policy Changes :\n")
+	fullDiff.WriteString("(+'s and -'s correspond to changes in policy staging diffed against the main policy state)\n")
+
+	for policyPointer < len(policyRules) || policyStagingPointer < len(policyStagingRules) {
+		if policyPointer == len(policyRules) {
+			fullDiff.WriteString(FindDiffBetweenRules("", GetListRulesString(policyStagingRules[policyStagingPointer].Delegation, policyStagingRules[policyStagingPointer].Depth)))
+			policyStagingPointer++
+		} else if policyStagingPointer == len(policyStagingRules) {
+			fullDiff.WriteString(FindDiffBetweenRules(GetListRulesString(policyRules[policyPointer].Delegation, policyRules[policyPointer].Depth), ""))
+			policyPointer++
+		} else if policyRules[policyPointer] != policyStagingRules[policyStagingPointer] {
+			if policyRules[policyPointer].Depth < policyStagingRules[policyStagingPointer].Depth {
+				fullDiff.WriteString(FindDiffBetweenRules("", GetListRulesString(policyStagingRules[policyStagingPointer].Delegation, policyStagingRules[policyStagingPointer].Depth)))
+				policyStagingPointer++
+			} else if policyRules[policyPointer].Depth > policyStagingRules[policyStagingPointer].Depth {
+				fullDiff.WriteString(FindDiffBetweenRules(GetListRulesString(policyRules[policyPointer].Delegation, policyRules[policyPointer].Depth), ""))
+				policyPointer++
+			} else {
+				fullDiff.WriteString(FindDiffBetweenRules(GetListRulesString(policyRules[policyPointer].Delegation, policyRules[policyPointer].Depth), GetListRulesString(policyStagingRules[policyStagingPointer].Delegation, policyStagingRules[policyStagingPointer].Depth)))
+				policyPointer++
+				policyStagingPointer++
+			}
+		}
+	}
+
+	fmt.Print(fullDiff.String())
+
 	return nil
+}
+
+func FindDiffBetweenRules(initial, withChanges string) string {
+	dmp := diffmatchpatch.New()
+	diffs := dmp.DiffMain(initial, withChanges, false)
+
+	lines1, lines2, lineArray := dmp.DiffLinesToChars(initial, withChanges)
+	diffs = dmp.DiffMain(lines1, lines2, false)
+	diffs = dmp.DiffCharsToLines(diffs, lineArray)
+
+	var diffDisplay strings.Builder
+
+	for _, diff := range diffs {
+		text := strings.TrimSuffix(diff.Text, "\n")
+		lines := strings.Split(text, "\n")
+
+		for _, line := range lines {
+			switch diff.Type {
+			case diffmatchpatch.DiffInsert:
+				diffDisplay.WriteString(fmt.Sprintf("\033[32m+   %s\033[0m\n", line)) // Green for additions
+			case diffmatchpatch.DiffDelete:
+				diffDisplay.WriteString(fmt.Sprintf("\033[31m-   %s\033[0m\n", line)) // Red for deletions
+			case diffmatchpatch.DiffEqual:
+				diffDisplay.WriteString(fmt.Sprintf("    %s\n", line))
+			}
+		}
+	}
+	return diffDisplay.String()
+}
+
+func GetListRulesString(rule tuf.Delegation, depth int) string {
+	var changes string
+
+	changes += fmt.Sprintf(strings.Repeat("    ", depth)+"Rule %s:\n", rule.Name)
+	gitpaths, filepaths := []string{}, []string{}
+	for _, path := range rule.Paths {
+		if strings.HasPrefix(path, "git:") {
+			gitpaths = append(gitpaths, path)
+		} else {
+			filepaths = append(filepaths, path)
+		}
+	}
+	if len(filepaths) > 0 {
+		changes += fmt.Sprintf(strings.Repeat("    ", depth+1) + "Paths affected:" + "\n")
+		for _, v := range filepaths {
+			changes += fmt.Sprintf(strings.Repeat("    ", depth+2)+"%s\n", v)
+		}
+	}
+	if len(gitpaths) > 0 {
+		changes += fmt.Sprintf(strings.Repeat("    ", depth+1) + "Refs affected:" + "\n")
+		for _, v := range gitpaths {
+			changes += fmt.Sprintf(strings.Repeat("    ", depth+2)+"%s\n", v)
+		}
+	}
+
+	changes += fmt.Sprintf(strings.Repeat("    ", depth+1) + "Authorized keys:" + "\n")
+	for _, key := range rule.Role.KeyIDs {
+		changes += fmt.Sprintf(strings.Repeat("    ", depth+2)+"%s\n", key)
+	}
+
+	changes += fmt.Sprintf(strings.Repeat("    ", depth+1) + fmt.Sprintf("Required valid signatures: %d", rule.Role.Threshold) + "\n")
+	return changes
 }
 
 func New() *cobra.Command {
