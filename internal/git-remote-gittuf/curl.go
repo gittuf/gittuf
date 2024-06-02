01007b71d8784d9b6e25c0142e6a85e679eede79
Aditya Sirish <aditya@saky.in>
2024-06-02 17:38:50 +0530

    transport: Add initial implementation
    Signed-off-by: Aditya Sirish <aditya@saky.in>

---
+++ internal/git-remote-gittuf/curl.go
@@ -0,0 +1,430 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/gittuf/gittuf/internal/rsl"
+)
+
+func handleCurl(remoteName, url string) (map[string]string, bool, error) {
+	helper := exec.Command("git-remote-http", remoteName, url)
+
+	helper.Stderr = os.Stderr
+
+	// We want to inspect the helper's stdout for the gittuf ref statuses
+	helperStdOutPipe, err := helper.StdoutPipe()
+	if err != nil {
+		return nil, false, err
+	}
+	helperStdOut := &logReadCloser{name: "git-remote-http stdout", readCloser: helperStdOutPipe}
+
+	// We want to interpose with the helper's stdin by passing in extra refs
+	// etc
+	helperStdInPipe, err := helper.StdinPipe()
+	if err != nil {
+		return nil, false, err
+	}
+	helperStdIn := &logWriteCloser{name: "git-remote-http stdin", writeCloser: helperStdInPipe}
+
+	if err := helper.Start(); err != nil {
+		return nil, false, err
+	}
+
+	stdInScanner := &logScanner{name: "git-remote-gittuf stdin", scanner: bufio.NewScanner(os.Stdin)}
+	stdInScanner.Split(splitInput)
+
+	var (
+		gittufRefsTips = map[string]string{}
+		pushCommands   = [][]byte{}
+		service        string
+		isPush         bool
+	)
+
+	currentState := start // top level "menu" for the helper
+	for stdInScanner.Scan() {
+		command := stdInScanner.Bytes()
+
+	alreadyScanned:
+
+		switch currentState {
+		case start:
+			log("state: start")
+			// Handle "top level" commands here
+			switch {
+			case bytes.HasPrefix(command, []byte("stateless-connect")):
+				/*
+					stateless-connect is the new experimental way of
+					communicating with the remote. It implements Git Protocol
+					v2. Here, we don't do much other than recognizing that we're
+					in a fetch, as this protocol doesn't support pushes yet.
+				*/
+				log("cmd: stateless-connect")
+				commandSplit := bytes.Split(bytes.TrimSpace(command), []byte(" "))
+				service = string(commandSplit[1])
+				log("found service", service)
+				currentState = serviceRouter // head to the service router next
+
+				if _, err := helperStdIn.Write(command); err != nil {
+					return nil, false, err
+				}
+
+				// Receive the initial info sent by the service
+				helperStdOutScanner := bufio.NewScanner(helperStdOut)
+				helperStdOutScanner.Split(splitOutput)
+
+				for helperStdOutScanner.Scan() {
+					output := helperStdOutScanner.Bytes()
+
+					if _, err := os.Stdout.Write(output); err != nil {
+						return nil, false, err
+					}
+
+					if bytes.Equal(output, flushPkt) {
+						break
+					}
+				}
+
+			case bytes.HasPrefix(command, []byte("list for-push")):
+				/*
+					The helper has two commands, in reality: list, list
+					for-push. Both of these are used to list the states of refs
+					on the remote. The for-push variation just formats it in a
+					way that can be used for the push comamnd later.
+
+					We inspect this to learn we're in a push. We also use the
+					output of this command, implemented by git-remote-https, to
+					learn what the states of the gittuf refs are on the remote.
+				*/
+				log("cmd: list for-push")
+				if _, err := helperStdIn.Write(command); err != nil {
+					return nil, false, err
+				}
+
+				helperStdOutScanner := bufio.NewScanner(helperStdOut)
+				helperStdOutScanner.Split(splitOutput)
+
+				log("list for-push returned:")
+				for helperStdOutScanner.Scan() {
+					output := helperStdOutScanner.Bytes()
+
+					refAdSplit := strings.Split(strings.TrimSpace(string(output)), " ")
+					if len(refAdSplit) >= 2 {
+						if strings.HasPrefix(refAdSplit[1], gittufRefPrefix) {
+							gittufRefsTips[refAdSplit[1]] = refAdSplit[0]
+						}
+					}
+
+					if _, err := os.Stdout.Write(output); err != nil {
+						return nil, false, err
+					}
+
+					if bytes.Equal(output, flushPkt) {
+						break
+					}
+				}
+
+			case bytes.HasPrefix(command, []byte("push")): // multiline input
+				log("cmd: push")
+				isPush = true
+
+				for {
+					if bytes.Equal(command, []byte("\n")) {
+						log("adding gittuf RSL entries if remote is gittuf-enabled")
+						// Fetch remote RSL if needed
+						// TODO
+						// cmd := exec.Command("git", "rev-parse", rsl.Ref)
+						// output, err := cmd.Output()
+						// if err != nil {
+						// 	return nil, false, err
+						// }
+						// localRSLTip := string(bytes.TrimSpace(output))
+						// remoteRSLTip := gittufRefsTips[rsl.Ref]
+						// if localRSLTip != remoteRSLTip {
+						// 	// TODO: This just assumes the local RSL is behind
+						// 	// the remote RSL. With the transport in use, the
+						// 	// local should never be ahead of remote, but we
+						// 	// should verify.
+
+						// 	var fetchStdOut bytes.Buffer
+						// 	cmd := exec.Command("git", "fetch", remoteName, fmt.Sprintf("%s:%s", rsl.Ref, rsl.Ref))
+						// 	cmd.Stdout = &fetchStdOut
+
+						// }
+
+						for _, pushCommand := range pushCommands {
+							if len(gittufRefsTips) != 0 {
+								refSpec := string(bytes.Split(bytes.TrimSpace(pushCommand), []byte{' '})[1])
+								refSpecSplit := strings.Split(refSpec, ":")
+								srcRef := refSpecSplit[0]
+								dstRef := refSpecSplit[1]
+
+								if !strings.HasPrefix(dstRef, gittufRefPrefix) {
+									// For all pushed refs that aren't gittuf
+									// refs, we create an RSL entry
+									cmd := exec.Command("gittuf", "rsl", "record", "--dst-ref", dstRef, srcRef)
+									cmd.Stderr = os.Stderr
+									cmd.Stdout = os.Stderr
+									if err := cmd.Run(); err != nil {
+										return nil, false, err
+									}
+								}
+							}
+
+							if _, err := helperStdIn.Write(pushCommand); err != nil {
+								return nil, false, err
+							}
+						}
+
+						// If remote is gittuf-enabled, also push the RSL
+						if len(gittufRefsTips) != 0 {
+							if _, err := helperStdIn.Write([]byte(fmt.Sprintf("push %s:%s\n", rsl.Ref, rsl.Ref))); err != nil {
+								return nil, false, err
+							}
+						}
+
+						// Add newline to indicate end of push batch
+						if _, err := helperStdIn.Write([]byte("\n")); err != nil {
+							return nil, false, err
+						}
+
+						break
+					}
+
+					pushCommands = append(pushCommands, command)
+
+					// Read in the next statement in the push batch
+					if !stdInScanner.Scan() {
+						// This should really not be reachable as we ought to
+						// get the newline and break first from our invoker.
+						break
+					}
+					command = stdInScanner.Bytes()
+				}
+
+				helperStdOutScanner := bufio.NewScanner(helperStdOut)
+				helperStdOutScanner.Split(splitOutput)
+
+				for helperStdOutScanner.Scan() {
+					output := helperStdOutScanner.Bytes()
+
+					if !bytes.Contains(output, []byte(gittufRefPrefix)) {
+						// we do this because git (at the very top level)
+						// inspects all the refs it's been asked to push and
+						// tracks their current status it never does this for
+						// the rsl ref, because only the transport is pushing
+						// that ref if we don't filter this out, it knows
+						// refs/gittuf/rsl got pushed, it knows _what_ the
+						// previous rsl tip was (by talking to the remote in
+						// list for-push) but it doesn't actually know the new
+						// tip of the rsl that was pushed because this is loaded
+						// before the transport is ever invoked.
+						if _, err := os.Stdout.Write(output); err != nil {
+							return nil, false, err
+						}
+					}
+
+					if bytes.Equal(output, flushPkt) {
+						break
+					}
+				}
+			default:
+				log("state: other-helper-command")
+				// Pass through other commands we don't want to interpose to the
+				// curl helper
+				if _, err := helperStdIn.Write(command); err != nil {
+					return nil, false, err
+				}
+
+				// Receive the initial info sent by the service
+				helperStdOutScanner := bufio.NewScanner(helperStdOut)
+				helperStdOutScanner.Split(splitOutput)
+
+				for helperStdOutScanner.Scan() {
+					output := helperStdOutScanner.Bytes()
+
+					if _, err := os.Stdout.Write(output); err != nil {
+						return nil, false, err
+					}
+
+					if bytes.Equal(output, flushPkt) {
+						break
+					}
+				}
+			}
+
+		case serviceRouter:
+			/*
+				The serviceRouter state is used when git-upload-pack is invoked
+				on the remote via stateless-connect. We've got nested states
+				here: git-upload-pack has commands you can pass to it, so
+				commands from this point onwards are not for the remote helper.
+			*/
+			log("state: service-router")
+			isPacketMode = true
+			switch service { //nolint:gocritic
+			case gitUploadPack: // fetching from remote
+				if bytes.Contains(command, []byte("command=ls-refs")) {
+					currentState = lsRefs
+				} else if bytes.Contains(command, []byte("command=fetch")) {
+					currentState = requestingWants
+				}
+				// Right now, only upload-pack can be handled this way
+			}
+
+			if _, err := helperStdIn.Write(command); err != nil {
+				return nil, false, err
+			}
+
+			// Right now, we don't need to wait for a response here, we check
+			// what command of the git service we're invoking and go to that
+			// state, this is almost a "routing" state. THIS MAY CHANGE!
+
+		case lsRefs:
+			/*
+				ls-refs is a command to upload-pack. Like list and list
+				for-push, it enumerates the refs and their states on the remote.
+				Unlike those commands, this must be passed to upload-pack.
+				Further, ls-refs must be parametrized with ref-prefixes. We add
+				refs/gittuf/ as a prefix to learn about the gittuf refs on the
+				remote during fetches.
+			*/
+			log("state: ls-refs")
+			if bytes.Equal(command, flushPkt) {
+				// add the gittuf ref-prefix right before the flushPkt
+				log("adding ref-prefix for refs/gittuf/")
+				gittufRefPrefixCommand := fmt.Sprintf("ref-prefix %s\n", gittufRefPrefix)
+				if _, err := helperStdIn.Write(packetEncode(gittufRefPrefixCommand)); err != nil {
+					return nil, false, err
+				}
+
+				currentState = lsRefsResponse
+			}
+
+			if _, err := helperStdIn.Write(command); err != nil {
+				return nil, false, err
+			}
+
+			// after writing flush to stdin, we can get the advertised refs
+			if currentState == lsRefsResponse {
+				helperStdOutScanner := bufio.NewScanner(helperStdOut)
+				helperStdOutScanner.Split(splitOutput)
+
+				for helperStdOutScanner.Scan() {
+					output := helperStdOutScanner.Bytes()
+
+					if !bytes.Equal(output, flushPkt) && !bytes.Equal(output, endOfReadPkt) {
+						refAd := strings.TrimSpace(string(output)[4:]) // remove the length prefix
+						if i := strings.IndexByte(refAd, '\x00'); i > 0 {
+							// this checks if the gittuf entry is the very first
+							// returned (unlikely because of HEAD)
+							refAd = refAd[:i] // drop everything from null byte onwards
+						}
+						refAdSplit := strings.Split(refAd, " ")
+
+						if strings.HasPrefix(refAdSplit[1], gittufRefPrefix) {
+							gittufRefsTips[refAdSplit[1]] = refAdSplit[0]
+						}
+					}
+
+					if _, err := os.Stdout.Write(output); err != nil {
+						return nil, false, err
+					}
+
+					if bytes.Equal(output, endOfReadPkt) {
+						break
+					}
+				}
+
+				currentState = serviceRouter // go back to service's "router"
+			}
+
+		case requestingWants:
+			/*
+				At this point, we enter the haves / wants negotiation, which is
+				followed usually by the remote sending a packfile with the
+				requested Git objects.
+
+				We add the gittuf specific objects as wants. We don't have to
+				specify haves as Git automatically specifies all the objects it
+				has regardless of what refs they're reachable via.
+			*/
+			log("state: requesting-wants")
+			wantsDone := false
+			if bytes.Equal(command, flushPkt) {
+				if !wantsDone {
+					// Write gittuf wants
+					log("adding gittuf wants")
+					for _, tip := range gittufRefsTips {
+						wantCmd := fmt.Sprintf("want %s\n", tip)
+						if _, err := helperStdIn.Write(packetEncode(wantCmd)); err != nil {
+							return nil, false, err
+						}
+					}
+					wantsDone = true
+
+					// FIXME: does this work for incremental fetches?
+					currentState = packfileIncoming
+				}
+			}
+
+			if _, err := helperStdIn.Write(command); err != nil {
+				return nil, false, err
+			}
+
+			if currentState == packfileIncoming {
+				log("awaiting packfile(s)")
+				helperStdOutScanner := bufio.NewScanner(helperStdOut)
+				helperStdOutScanner.Split(splitOutput)
+
+				// TODO: fix issues with multiplexing
+				for helperStdOutScanner.Scan() {
+					output := helperStdOutScanner.Bytes()
+
+					if _, err := os.Stdout.Write(output); err != nil {
+						return nil, false, err
+					}
+
+					if bytes.Equal(output, endOfReadPkt) {
+						if !stdInScanner.Scan() {
+							break
+						}
+						command = stdInScanner.Bytes()
+						if len(command) == 0 {
+							break
+						}
+						// we have a second want batch
+						currentState = requestingWants
+						goto alreadyScanned
+					}
+				}
+				if currentState == packfileIncoming {
+					currentState = packfileDone
+				}
+			}
+		}
+		if currentState == packfileDone {
+			break
+		}
+	}
+
+	if err := helperStdIn.Close(); err != nil {
+		return nil, false, err
+	}
+
+	if err := helperStdOut.Close(); err != nil {
+		return nil, false, err
+	}
+
+	if err := helper.Wait(); err != nil {
+		return nil, false, err
+	}
+
+	return gittufRefsTips, isPush, nil
+}
---
+++ internal/git-remote-gittuf/main.go
@@ -0,0 +1,248 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+/*
+	Git supports the idea of "remote helpers" which can be used to modify
+	interactions with remote repositories.
+	https://git-scm.com/docs/gitremote-helpers
+
+	Interactions with a remote take two forms: either we're fetching objects
+	(git fetch, pull, clone) or we're sending objects (git push). Git has a
+	custom protocol dictating the interactions for fetches and pushes and how
+	data is communicated using "packfiles". Note: this protocol is now
+	versioned. v0 and v1 are effectively identical except that v1 has an
+	explicit version declaration while v0 has nothing.
+	https://git-scm.com/docs/pack-protocol
+
+	On the other hand, v2 is a significant departure, and is documented
+	separately. Currently, v2 only supports fetches, with pushes using v0/v1.
+	https://git-scm.com/docs/protocol-v2
+
+	In both cases, there's an underlying communication protocol. Overwhelmingly,
+	this is ssh in the real world, to the point that it is part of the Git
+	implementation itself. In contrast, the HTTP(s) / FTP(s) protocols are
+	implemented as a remote helper program using curl.
+
+	Setup:
+		* git-remote-gittuf must be in PATH.
+		* A remote must be configured to use `gittuf::` as the prefix. The
+		  result of the remote URL must indicate the underlying transport
+		  mechanism. Example: `gittuf::https://github.com/gittuf/gittuf` and
+		  `gittuf::git@github.com:gittuf/gittuf`
+
+	Invocation:
+	During an interaction with a remote configured using the gittuf:: prefix,
+	Git invokes the gittuf helper from the PATH.
+
+	Initial interaction:
+	Git learns what capabilities the remote helper implements, and chooses the
+	appropriate one for the task at hand. git-remote-gittuf is not consistent in
+	the advertised capabilities. This is because when the underlying transport
+	is HTTP(s) / FTP(s), we just invoke git-remote-http and relay its
+	capabilities back to Git. When the underlying transport is SSH, we advertise
+	a lightweight set of capabilities to ensure Git chooses the one we want it
+	to for both cases.
+
+	Anatomy of a fetch:
+		* Git invokes stateless-connect (protocol v2) to communicate with
+		  git-upload-pack on the remote
+		* Git invokes ls-refs, a command of git-upload-pack, to learn what refs
+		  the remote has and what their tips point to
+		  Note: we interpose this to learn the status of gittuf refs on the
+		  remote
+		* Git negotiates with git-upload-pack the objects it wants based on the
+		  refs that must be fetched
+		  Note: we interpose this to request gittuf specific objects as well
+		* The remote sends a packfile with the requested objects
+		* git-remote-gittuf uses update-ref to set the local gittuf refs, as Git
+		  will not do this for us
+
+	Anatomy of a push:
+		* Git invokes list for-push (protocol v0/v1) to list the refs available
+		  on the remote
+		* Git invokes push (protocol v0/v1) to indicate what refs must be
+		  updated to on the remote
+		* A packfile is created and streamed to git-receive-pack on the server
+*/
+
+var (
+	// logFile is used to debug git-remote-gittuf. It is set using
+	// GITTUF_LOG_FILE.
+	logFile io.Writer
+
+	// gitVersion contains the version of Git used by the client invoking
+	// git-remote-gittuf. It is used to self-identify with a remote service such
+	// as git-upload-pack and git-receive-pack.
+	gitVersion string
+
+	isPacketMode = false
+
+	flushPkt     = []byte{'0', '0', '0', '0'}
+	delimiterPkt = []byte{'0', '0', '0', '1'}
+	endOfReadPkt = []byte{'0', '0', '0', '2'}
+
+	zeroHash = "0000000000000000000000000000000000000000"
+)
+
+type state int
+
+// TODO: we have a mix of states here, lsRefs onwards are when interacting with
+// upload-pack.
+const (
+	start                   state = iota // top level menu of helper, serves as router too when not within a service
+	serviceRouter                        // router for service
+	lsRefs                               // upload-pack
+	lsRefsResponse                       // upload-pack
+	requestingWants                      // upload-pack
+	requestingWantsResponse              // upload-pack
+	packfileIncoming                     // upload-pack
+	packfileDone
+)
+
+const (
+	gitUploadPack   = "git-upload-pack"
+	gitReceivePack  = "git-receive-pack"
+	gittufRefPrefix = "refs/gittuf/"
+)
+
+func run() error {
+	if len(os.Args) < 3 {
+		return fmt.Errorf("usage: %s <remote-name> <url>", os.Args[0])
+	}
+
+	gitDir := os.Getenv("GIT_DIR")
+	remoteName := os.Args[1]
+	url := os.Args[2]
+
+	var handler func(string, string) (map[string]string, bool, error)
+	switch {
+	case strings.HasPrefix(url, "https://"), strings.HasPrefix(url, "http://"), strings.HasPrefix(url, "ftp://"), strings.HasPrefix(url, "ftps://"):
+		log("Prefix indicates curl remote helper must be used")
+		handler = handleCurl
+	case strings.HasPrefix(url, "/"), strings.HasPrefix(url, "file://"):
+		log("Prefix indicates file helper must be used")
+		return nil
+	default:
+		log("Using ssh helper")
+		handler = handleSSH
+	}
+
+	// var rslTip string
+	// cmd := exec.Command("git", "--git-dir", gitDir, "rev-parse", rsl.Ref) //nolint:gosec
+	// rslTipB, err := cmd.Output()
+	// if err == nil {
+	// 	rslTip = string(bytes.TrimSpace(rslTipB))
+	// }
+
+	gittufRefsTips, isPush, err := handler(remoteName, url)
+	if err != nil {
+		return err
+	}
+
+	for {
+		// When cloning/fetching, we have to hang until Git sets things up
+		// before we can update-ref
+		entries, err := os.ReadDir(filepath.Join(gitDir, "objects", "pack"))
+		if err != nil {
+			return err
+		}
+
+		if len(entries) == 0 {
+			break
+		}
+
+		// `entries` is sorted by name. The "regular" entries have pack- as a
+		// prefix. When actually fetching contents, it's stored in a tmp_pack or
+		// temp_rev file. Therefore, if the last entry starts with pack-, we
+		// know we don't have a tmp_ file.
+
+		lastEntryName := entries[len(entries)-1].Name()
+		if strings.HasPrefix(lastEntryName, "pack-") { // not tmp_pack or tmp_rev
+			break
+		}
+	}
+
+	if !isPush {
+		// TODO: this breaks when `git fetch` is invoked explicitly for a gittuf
+		// ref because Git separately tries to update-ref.
+		// During wants, check if the latest remote gittuf ref tips are
+		// requested. Use _only_ the latest so as to avoid any unnecessary blob
+		// collissions.
+		for ref, tip := range gittufRefsTips {
+			args := []string{"--git-dir", gitDir, "update-ref", ref, tip}
+			cmd := exec.Command("git", args...)
+
+			if err := cmd.Run(); err != nil {
+				return fmt.Errorf("unable to set '%s' to '%s'", ref, tip)
+			}
+		}
+
+		// Uncomment after gittuf can accept a git_dir env var; this will happen
+		// with the gitinterface PRs naturally.
+
+		// TODO: this must either be looped to address each changed ref that
+		// exists locally or gittuf needs another flag for --all.
+		// var cmd *exec.Cmd
+		// if rslTip != "" {
+		// 	log("we have rsl tip")
+		// 	cmd = exec.Command("gittuf", "verify-ref", "--from-entry", rslTip, "HEAD")
+		// } else {
+		// 	cwd, _ := os.Getwd()
+		// 	log("we don't have rsl tip", cwd)
+		// 	cmd = exec.Command("gittuf", "verify-ref", "HEAD")
+		// }
+		// _, err := cmd.Output()
+		// if err != nil {
+		// 	log(err.Error())
+		// 	if _, nerr := os.Stderr.Write([]byte("gittuf verification failed\n")); nerr != nil {
+		// 		return errors.Join(err, nerr)
+		// 	}
+		// 	return err
+		// }
+	}
+
+	return nil
+}
+
+func populateGitVersion() error {
+	cmd := exec.Command("git", "--version")
+	output, err := cmd.Output()
+	if err != nil {
+		return err
+	}
+
+	gitVersion = strings.TrimPrefix(strings.TrimSpace(string(output)), "git version ")
+	return nil
+}
+
+func main() {
+	logFilePath := os.Getenv("GITTUF_LOG_FILE")
+	if logFilePath != "" {
+		file, err := os.Create(logFilePath)
+		if err != nil {
+			panic(err)
+		}
+
+		logFile = file
+	}
+
+	if err := populateGitVersion(); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	if err := run(); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+}
