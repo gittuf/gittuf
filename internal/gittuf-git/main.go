a45548eaca2eaf476a9ecafa9252d492d5955d08
Raghava Gatadi <raghava.gtd@gmail.com>
2024-06-21 23:17:06 +0530

    using os library instead of cobra
    Signed-off-by: Raghava Gatadi <raghava.gtd@gmail.com>

--- internal/gittuf-git/main.go
+++ internal/gittuf-git/main.go
@@ -3,15 +3,41 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"os"
+	"os/exec"
 )
 
 func main() {
-	Cmd := New()
-	if err := Cmd.Execute(); err != nil {
-		// We can ignore the linter here (deferred functions are not executed
-		// when os.Exit is invoked) because if we do have an error, we don't
-		// have a panic, which is what the deferred function is looking for.
-		os.Exit(1) //nolint:gocritic
+	// Check if enough arguments are provided
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: gittuf-git <git-command> [<args>]")
+		gitCmd := exec.Command("git", "--help")
+		gitCmd.Stdout = os.Stdout
+		gitCmd.Stderr = os.Stderr
+		err := gitCmd.Run()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error running git help command: %v\n", err)
+		}
+		os.Exit(1)
+	}
+
+	gitArgs := os.Args[1:]
+
+	// Execute the git command
+	gitCmd := exec.Command("git", gitArgs...)
+	gitCmd.Stdout = os.Stdout
+	gitCmd.Stderr = os.Stderr
+
+	err := gitCmd.Run()
+	if err != nil {
+		var exitErr *exec.ExitError
+		if errors.As(err, &exitErr) {
+			fmt.Fprintln(os.Stderr, string(exitErr.Stderr)) // Output to stderr
+		} else {
+			fmt.Fprintf(os.Stderr, "Error running git command: %v\n", err)
+		}
+		os.Exit(1)
 	}
 }
