a40164eae382c69d3736f255b184af62a764cb5e
Raghava Gatadi <raghava.gtd@gmail.com>
2024-06-26 18:48:56 +0530

    added a check to determine rebase has taken place
    Signed-off-by: Raghava Gatadi <raghava.gtd@gmail.com>

--- internal/gittuf-git/push.go
+++ internal/gittuf-git/push.go
@@ -3,9 +3,10 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"strings"
 
-	"github.com/gittuf/gittuf/internal/cmd/rsl"
+	"github.com/gittuf/gittuf/internal/cmd/rsl/record"
 )
 
 func splitPushArgs(args []string) (rslRecordArgs []string) {
@@ -13,7 +14,9 @@ func splitPushArgs(args []string) (rslRecordArgs []string) {
 
 	for _, arg := range args {
 		if !strings.HasPrefix(arg, "-") {
-			rslRecordArgs = append(rslRecordArgs, arg)
+			if arg != "origin" {
+				rslRecordArgs = append(rslRecordArgs, arg)
+			}
 		}
 	}
 	return rslRecordArgs
@@ -21,12 +24,32 @@ func splitPushArgs(args []string) (rslRecordArgs []string) {
 
 func gittufPush(gitArgs []string) {
 	rslRecordArgs := splitPushArgs(gitArgs[1:])
-	recordCmd := rsl.New()
+	recordCmd := record.New()
 	rslArg := []string{}
 	for _, arg := range rslRecordArgs {
 		rslArg = append(rslArg, arg)
+		hasRebased, err := hasRefBeenRebased(arg)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "failed to check for rebase: %v", err)
+		}
+		if hasRebased {
+			rslArg = append(rslArg, "--skip-rewritten")
+		}
+		// rsl record command
 		if err := recordCmd.RunE(nil, rslArg); err != nil {
 			fmt.Fprintf(os.Stderr, "failed to record RSL entry: %v\n", err)
 		}
 	}
+}
+
+func hasRefBeenRebased(ref string) (bool, error) {
+	cmd := exec.Command("git", "log", "--left-right", "--cherry-pick", "origin/"+ref+"..."+ref) // #nosec
+	output, err := cmd.Output()
+	if err != nil {
+		return false, fmt.Errorf("failed to check for rebase: %w", err)
+	}
+	// If there is output, it means there are unique commits on the local branch that haven't
+	// been merged to the remote, indicating a rebase
+
+	return len(output) > 0, nil
 }
