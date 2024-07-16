a8a2004486ec9630bb61eb1f3a80172fa0122551
Aditya Sirish A Yelgundhalli <ayelgundhall@bloomberg.net>
2024-07-16 12:21:53 -0400

    transport: Fetch refs/gittuf/* before creating entries
    Signed-off-by: Aditya Sirish A Yelgundhalli <ayelgundhall@bloomberg.net>

--- internal/git-remote-gittuf/ssh.go
+++ internal/git-remote-gittuf/ssh.go
@@ -356,6 +356,15 @@ func handleSSH(_, url string) (map[string]string, bool, error) {
 					command = stdInScanner.Bytes()
 				}
 
+				log("fetching remote gittuf refs")
+				cmd := exec.Command("git", "fetch", url, "refs/gittuf/*:refs/gittuf/*")
+				cmd.Stderr = os.Stderr
+				cmd.Stdout = os.Stderr
+				cmd.Stdin = os.Stdin
+				if err := cmd.Run(); err != nil {
+					return nil, false, err
+				}
+
 				pushObjects := set.NewSet[string]()
 				log("adding gittuf RSL entries")
 				for i, refSpec := range pushRefSpecs {
@@ -426,7 +435,7 @@ func handleSSH(_, url string) (map[string]string, bool, error) {
 				// TODO: gittuf verify-ref for each dstRef; abort if
 				// verification fails
 
-				cmd := exec.Command("git", "rev-parse", rsl.Ref) //nolint:gosec
+				cmd = exec.Command("git", "rev-parse", rsl.Ref) //nolint:gosec
 				output, err := cmd.Output()
 				if err != nil {
 					return nil, false, err
