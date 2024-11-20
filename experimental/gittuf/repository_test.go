57af4f16ff03f1308544ff06c90ba6230357e830
Yongjae Chung <yongjae354@berkeley.edu>
2024-11-20 14:53:36 -0500

    gittuf: Improve testing coverage repository_test
    Signed-off-by: Yongjae Chung <yongjae354@berkeley.edu>

--- experimental/gittuf/repository_test.go
+++ experimental/gittuf/repository_test.go
@@ -13,21 +13,38 @@ import (
 )
 
 func TestLoadRepository(t *testing.T) {
-	tmpDir := t.TempDir()
-	gitinterface.CreateTestGitRepository(t, tmpDir, false)
+	t.Run("load with no repo, unsuccessful", func(t *testing.T) {
+		tmpDir := t.TempDir()
+		currentDir, err := os.Getwd()
+		if err != nil {
+			t.Fatal(err)
+		}
+		if err := os.Chdir(tmpDir); err != nil {
+			t.Fatal(err)
+		}
+		defer os.Chdir(currentDir) //nolint:errcheck
 
-	currentDir, err := os.Getwd()
-	if err != nil {
-		t.Fatal(err)
-	}
-	if err := os.Chdir(tmpDir); err != nil {
-		t.Fatal(err)
-	}
-	defer os.Chdir(currentDir) //nolint:errcheck
+		repository, err := LoadRepository()
+		assert.NotNil(t, err)
+		assert.Nil(t, repository)
+	})
 
-	repository, err := LoadRepository()
-	assert.Nil(t, err)
-	assert.NotNil(t, repository.r)
+	t.Run("successful load", func(t *testing.T) {
+		tmpDir := t.TempDir()
+		currentDir, err := os.Getwd()
+		if err != nil {
+			t.Fatal(err)
+		}
+		if err := os.Chdir(tmpDir); err != nil {
+			t.Fatal(err)
+		}
+		defer os.Chdir(currentDir) //nolint:errcheck
+
+		gitinterface.CreateTestGitRepository(t, tmpDir, false)
+		repository, err := LoadRepository()
+		assert.Nil(t, err)
+		assert.NotNil(t, repository.r)
+	})
 }
 
 func TestUnauthorizedKey(t *testing.T) {
@@ -53,4 +70,13 @@ func TestUnauthorizedKey(t *testing.T) {
 		err := r.RemoveTopLevelTargetsKey(testCtx, targetsSigner, "some key ID", false)
 		assert.ErrorIs(t, err, ErrUnauthorizedKey)
 	})
+}
+
+func TestGetGitRepository(t *testing.T) {
+	tmpDir := t.TempDir()
+	repo := gitinterface.CreateTestGitRepository(t, tmpDir, false)
+
+	r := &Repository{r: repo}
+	result := r.GetGitRepository()
+	assert.Equal(t, repo, result)
 }
