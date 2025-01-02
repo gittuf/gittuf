b0504ae75e670217ff26876b5b61655754d48c8b
Pat Zielinski <70954403+patzielinski@users.noreply.github.com>
2025-01-02 09:43:34 -0500

    *: Add tests in policy and gitinterface
    Co-authored-by: Raghava Gatadi <raghava.gtd@gmail.com>
    Signed-off-by: Pat Zielinski <70954403+patzielinski@users.noreply.github.com>

--- internal/policy/root_test.go
+++ internal/policy/root_test.go
@@ -29,3 +29,45 @@ func TestInitializeRootMetadata(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, []tuf.Principal{key}, rootPrincipals)
 }
+
+func TestUpdateRootThreshold(t *testing.T) {
+	key := tufv01.NewKeyFromSSLibKey(ssh.NewKeyFromBytes(t, rootPubKeyBytes))
+
+	rootMetadata, err := InitializeRootMetadata(key)
+	assert.Nil(t, err)
+
+	newRootKey1 := tufv01.NewKeyFromSSLibKey(ssh.NewKeyFromBytes(t, targets1PubKeyBytes))
+	err = rootMetadata.AddRootPrincipal(newRootKey1)
+	assert.Nil(t, err)
+
+	newRootKey2 := tufv01.NewKeyFromSSLibKey(ssh.NewKeyFromBytes(t, targets2PubKeyBytes))
+	err = rootMetadata.AddRootPrincipal(newRootKey2)
+	assert.Nil(t, err)
+
+	err = rootMetadata.UpdateRootThreshold(4)
+	assert.ErrorIs(t, err, tuf.ErrCannotMeetThreshold)
+
+	err = rootMetadata.UpdateRootThreshold(2)
+	assert.Nil(t, err)
+}
+
+func TestUpdatePolicyThreshold(t *testing.T) {
+	key := tufv01.NewKeyFromSSLibKey(ssh.NewKeyFromBytes(t, rootPubKeyBytes))
+
+	rootMetadata, err := InitializeRootMetadata(key)
+	assert.Nil(t, err)
+
+	targetsKey1 := tufv01.NewKeyFromSSLibKey(ssh.NewKeyFromBytes(t, targets1PubKeyBytes))
+	err = rootMetadata.AddPrimaryRuleFilePrincipal(targetsKey1)
+	assert.Nil(t, err)
+
+	targetsKey2 := tufv01.NewKeyFromSSLibKey(ssh.NewKeyFromBytes(t, targets2PubKeyBytes))
+	err = rootMetadata.AddPrimaryRuleFilePrincipal(targetsKey2)
+	assert.Nil(t, err)
+
+	err = rootMetadata.UpdatePrimaryRuleFileThreshold(4)
+	assert.ErrorIs(t, err, tuf.ErrCannotMeetThreshold)
+
+	err = rootMetadata.UpdatePrimaryRuleFileThreshold(2)
+	assert.Nil(t, err)
+}
