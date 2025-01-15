9714efe4412ae4cd1be1164f70c7a4f700625d8f
James Sun <sunzhongrui66@gmail.com>
2025-01-14 22:54:26 -0500

    Update run-tests3.sh

--- run-tests3.sh
+++ run-tests3.sh
@@ -7,7 +7,7 @@ set -u  # Treat unset variables as an error
 # Constants
 GITTUF_BINARY="git-remote-gittuf"
 TEST_REPO_URL_SSH="gittuf::git@github.com:gittuf/zsun6"
-TEST_REPO_URL_HTTPS="gittuf::https://github.com/zsun6/gittuf"
+TEST_REPO_URL_HTTPS="gittuf::https://github.com/zsun6/gittuf-remote-test_repo"
 TEST_CLONE_DIR="test_clone"
 TEST_REMOTE_NAME="origin"
 GITTUF_TEST_BRANCH="test-branch"
@@ -52,22 +52,56 @@ if [ "$REMOTE_URL" != "$TEST_REPO_URL_HTTPS" ]; then
 fi
 echo "Successfully set remote URL to HTTPS."
 
+# Test first fetch
+echo "Testing fetch operation..."
+git fetch "$TEST_REMOTE_NAME"
+if [ $? -ne 0 ]; then
+    echo "Error: Fetch operation failed."
+    exit 1
+fi
+# Navigate to the folder where the repo is cloned
+REPO_DIR="repo2"
+
+# Check if the directory exists
+if [ -d "$REPO_DIR" ]; then
+    echo "Repository folder '$REPO_DIR' found."
+    # Check for the README file inside the repo directory
+    README_FILE="$REPO_DIR/README.md"
+    if [ -f "$README_FILE" ]; then
+        echo "README file found. Printing contents:"
+        cat "$README_FILE"
+    else
+        echo "README file not found in '$REPO_DIR'."
+    fi
+else
+    echo "Repository folder '$REPO_DIR' does not exist."
+    exit 1
+fi
+echo "Fetch operation completed successfully."
+
 # Test creating a new branch and pushing changes
 echo "Testing push operation..."
 git checkout -b "$GITTUF_TEST_BRANCH"
-echo "Test file" > test_file.txt
-git add test_file.txt
+echo "This is auto test" > written_by_autotest.txt
+git add written_by_autotest.txt
 git commit -m "Add test file for $GITTUF_TEST_BRANCH"
 git push -u "$TEST_REMOTE_NAME" "$GITTUF_TEST_BRANCH" --force
 
 echo "Push operation completed successfully."
 
+README_FILE_2="written_by_autotest.txt"
 # Test fetching updates
 echo "Testing fetch operation..."
 git fetch "$TEST_REMOTE_NAME"
 if [ $? -ne 0 ]; then
     echo "Error: Fetch operation failed."
     exit 1
+fi
+if [ -f "$README_FILE_2" ]; then
+    echo "pushed file found. Printing contents:"
+    cat "$README_FILE_2"
+else
+    echo "README file not found in '$REPO_DIR'."
 fi
 echo "Fetch operation completed successfully."
 
