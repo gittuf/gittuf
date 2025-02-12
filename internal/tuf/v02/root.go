907475b40702f9354b36a505a4769699874d54c7
乔晖 <qh89@10-18-183-46.dynapool.wireless.nyu.edu>
2025-02-12 10:49:13 -0500

    rebase to fix DCO
    Signed-off-by: 乔晖 qh89@10-18-183-46.dynapool.wireless.nyu.edu
    Signed-off-by: QH89 <hq2139@nyu.edu>

--- internal/tuf/v02/root.go
+++ internal/tuf/v02/root.go
@@ -478,8 +478,8 @@ func (r *RootMetadata) UpdatePropagationDirective(directive tuf.PropagationDirec
 		}
 	}
 
-	//if the directive is new, call add propagation directive to deal with it
-	if err := r.AddPropagationDirective(directive); err != nil{
+	// if the directive is new, call add propagation directive to deal with it
+	if err := r.AddPropagationDirective(directive); err != nil {
 		return err
 	}
 	return nil
