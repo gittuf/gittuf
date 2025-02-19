932aa3dec066cbfa619e45178c33b2826f48e744
Yongjae Chung <yongjae354@berkeley.edu>
2025-02-19 13:34:06 -0500

    add command flow
    Signed-off-by: Yongjae Chung <yongjae354@berkeley.edu>

---
+++ docs/cli/gittuf_trust_list-globalrules.md
@@ -0,0 +1,30 @@
+## gittuf trust list-globalrules
+
+List global rules for the current state
+
+```
+gittuf trust list-globalrules [flags]
+```
+
+### Options
+
+```
+  -h, --help                help for list-globalrules
+      --target-ref string   specify which policy ref should be inspected (default "policy")
+```
+
+### Options inherited from parent commands
+
+```
+      --no-color                     turn off colored output
+      --profile                      enable CPU and memory profiling
+      --profile-CPU-file string      file to store CPU profile (default "cpu.prof")
+      --profile-memory-file string   file to store memory profile (default "memory.prof")
+  -k, --signing-key string           signing key to use to sign root of trust
+      --verbose                      enable verbose logging
+```
+
+### SEE ALSO
+
+* [gittuf trust](gittuf_trust.md)	 - Tools for gittuf's root of trust
+
---
+++ internal/cmd/trust/listglobalrules/listglobalrules.go
@@ -0,0 +1,76 @@
+// Copyright The gittuf Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package listglobalrules
+
+import (
+	"fmt"
+
+	"github.com/gittuf/gittuf/experimental/gittuf"
+	"github.com/gittuf/gittuf/internal/policy"
+	"github.com/gittuf/gittuf/internal/tuf"
+	"github.com/spf13/cobra"
+)
+
+const indentString = "    "
+
+type options struct {
+	targetRef string
+}
+
+func (o *options) AddFlags(cmd *cobra.Command) {
+	cmd.Flags().StringVar(
+		&o.targetRef,
+		"target-ref",
+		"policy",
+		"specify which policy ref should be inspected",
+	)
+}
+
+func (o *options) Run(cmd *cobra.Command, _ []string) error {
+	repo, err := gittuf.LoadRepository()
+	if err != nil {
+		return err
+	}
+
+	rules, err := repo.ListGlobalRules(cmd.Context(), policy.PolicyRef)
+	if len(rules) == 0 {
+		fmt.Println("The rules slice is empty")
+	}
+	if err != nil {
+		return err
+	}
+
+	for _, curRule := range rules {
+		fmt.Printf("GlobalRule: %v\n", curRule.GetName())
+
+		switch rule := curRule.(type) {
+		case tuf.GlobalRuleThreshold:
+			fmt.Println(indentString + "GlobalRule Type: GlobalRuleThreshold")
+			fmt.Printf(indentString+"Refs affected: %v\n", rule.GetProtectedNamespaces())
+			fmt.Printf(indentString+"Threshold: %d\n", rule.GetThreshold())
+
+		case tuf.GlobalRuleBlockForcePushes:
+			fmt.Println(indentString + "GlobalRule Type: GlobalRuleBlockForcePushes")
+			fmt.Printf(indentString+"Refs affected: %v\n", rule.GetProtectedNamespaces())
+
+		default:
+			return tuf.ErrUnknownGlobalRuleType
+		}
+	}
+
+	return nil
+}
+
+func New() *cobra.Command {
+	o := &options{}
+	cmd := &cobra.Command{
+		Use:               "list-globalrules",
+		Short:             "List global rules for the current state",
+		RunE:              o.Run,
+		DisableAutoGenTag: true,
+	}
+	o.AddFlags(cmd)
+
+	return cmd
+}
