099feb57cb681f3a686c88d651833152bf46042e
Pat Zielinski <70954403+patzielinski@users.noreply.github.com>
2025-04-03 22:54:48 -0400

    wip
    Signed-off-by: Pat Zielinski <70954403+patzielinski@users.noreply.github.com>

--- internal/signerverifier/gpg/gpg.go
+++ internal/signerverifier/gpg/gpg.go
@@ -5,6 +5,8 @@ package gpg
 
 import (
 	"bytes"
+	"context"
+	"crypto"
 	"fmt"
 	"strings"
 
@@ -13,6 +15,41 @@ import (
 )
 
 const KeyType = "gpg"
+
+// Verifier is a dsse.Verifier implementation for GPG keys.
+type Verifier struct {
+	keyID   string
+	gpgKey  openpgp.Key
+	keyring openpgp.EntityList
+}
+
+// Verify implements the dsse.Verifier.Verify interface for GPG keys.
+func (v *Verifier) Verify(_ context.Context, data []byte, sig []byte) error {
+	openpgp.
+		keyring, err := openpgp.ReadArmoredKeyRing(bytes.NewReader(contents))
+	if err != nil {
+		return err
+	}
+
+	_, signer, err := openpgp.VerifyDetachedSignature()
+	if err != nil {
+		return fmt.Errorf("failed to verify gpg signature: %w", err)
+	}
+
+	return nil
+}
+
+// KeyID implements the dsse.Verifier.KeyID interface for GPG keys.
+// FIXME: consider removing error in interface; a dsse.Verifier needs a keyid
+func (v *Verifier) KeyID() (string, error) {
+	return v.keyID, nil
+}
+
+// Public implements the dsse.Verifier.Public interface for GPG keys.
+// FIXME: consider removing in interface, "Verify()" is all that's needed
+func (v *Verifier) Public() crypto.PublicKey {
+	return v.gpgKey.PublicKey
+}
 
 // LoadGPGKeyFromBytes returns a signerverifier.SSLibKey for a GPG / PGP key passed in as
 // armored bytes. The returned signerverifier.SSLibKey uses the primary key's fingerprint as the
@@ -37,4 +74,12 @@ func LoadGPGKeyFromBytes(contents []byte) (*signerverifier.SSLibKey, error) {
 	}
 
 	return gpgKey, nil
+}
+
+// NewVerifierFromKey creates a new Verifier from SSlibKey of type GPG.
+func NewVerifierFromKey(key *signerverifier.SSLibKey) (*Verifier, error) {
+	if key.KeyType != KeyType {
+		return nil, fmt.Errorf("wrong keyType: %s", key.KeyType)
+	}
+	gpgKey, err := key.KeyVal
 }
