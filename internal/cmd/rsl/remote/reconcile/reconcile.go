4a93b241cd44ca34546dc0117d01ee56c26ff60f
Syed Mohammed Sylani <sylanmohammed@gmail.com>
2025-06-12 19:23:01 +0530

    docs: simplify Long descriptions for reconcile and add-github-app commands
    Signed-off-by: Syed Mohammed Sylani <sylanmohammed@gmail.com>

--- internal/cmd/rsl/remote/reconcile/reconcile.go
+++ internal/cmd/rsl/remote/reconcile/reconcile.go
@@ -24,7 +24,7 @@ func New() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:               "reconcile <remote>",
 		Short:             "Reconcile local RSL with remote RSL",
-		Long:              `This command checks the local RSL against the specified remote and reconciles the local RSL if needed. If the local RSL doesn't exist or is strictly behind the remote RSL, then the local RSL is updated to match the remote RSL. If the local RSL is ahead of the remote RSL, nothing is updated. Finally, if the local and remote RSLs have diverged, then the local only RSL entries are reapplied over the latest entries in the remote if the local only RSL entries and remote only entries are for different Git references.`,
+		Long:              "Reconcile your local Repository Signing Log (RSL) with a remote, updating local history only when behind or non-diverged.",
 		Args:              cobra.ExactArgs(1),
 		RunE:              o.Run,
 		DisableAutoGenTag: true,
