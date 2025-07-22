1cab1b8ada0d101992eb7cfa22edcccfda26e8ab
Syed Mohammed Sylani <sylanmohammed@gmail.com>
2025-07-22 12:49:59 +0530

    docs: add Long description to cache main command
    Signed-off-by: Syed Mohammed Sylani <sylanmohammed@gmail.com>

--- internal/cmd/cache/cache.go
+++ internal/cmd/cache/cache.go
@@ -13,6 +13,7 @@ func New() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:               "cache",
 		Short:             "Manage gittuf's caching functionality",
+		Long:              `The 'cache' command group contains subcommands to manage gittuf's local persistent cache. This cache helps improve performance by storing metadata locally. The cache is local-only and is not synchronized with remote repositories.`,
 		DisableAutoGenTag: true,
 	}
 
