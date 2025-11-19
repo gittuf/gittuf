30142e3d2d3f64a5a93ec179c557fff612b5d7ab
Yongjae Chung <yc3028@nyu.edu>
2025-11-19 17:00:41 -0500

    add migration functions
    Signed-off-by: Yongjae Chung <yc3028@nyu.edu>

--- internal/policy/targets.go
+++ internal/policy/targets.go
@@ -8,11 +8,17 @@ import (
 
 	"github.com/gittuf/gittuf/internal/tuf"
 	tufv02 "github.com/gittuf/gittuf/internal/tuf/v02"
+	tufv03 "github.com/gittuf/gittuf/internal/tuf/v03"
 )
 
 // InitializeTargetsMetadata creates a new instance of TargetsMetadata.
 func InitializeTargetsMetadata() tuf.TargetsMetadata {
-	targetsMetadata := tufv02.NewTargetsMetadata()
+	var targetsMetadata tuf.TargetsMetadata
+	if tufv03.AllowV03Metadata() {
+		targetsMetadata = tufv03.NewTargetsMetadata()
+	} else {
+		targetsMetadata = tufv02.NewTargetsMetadata()
+	}
 
 	targetsMetadata.SetExpires(time.Now().AddDate(1, 0, 0).Format(time.RFC3339))
 	return targetsMetadata
