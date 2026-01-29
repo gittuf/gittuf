b29195a1636864165fa58f6b2ca8956d4ebf5838
rithwik0908 <rithwik.amajala@gmail.com>
2026-01-28 22:51:10 -0500

    trust: add add-root-person command
    Signed-off-by: rithwik0908 <rithwik.amajala@gmail.com>

---
+++ internal/cmd/trust/addrootperson.go
@@ -0,0 +1,126 @@
+// Copyright The gittuf Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package trust
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/gittuf/gittuf/experimental/gittuf"
+	trustpolicyopts "github.com/gittuf/gittuf/experimental/gittuf/options/trustpolicy"
+	"github.com/gittuf/gittuf/internal/cmd/trust/persistent"
+	"github.com/gittuf/gittuf/internal/tuf"
+	tufv02 "github.com/gittuf/gittuf/internal/tuf/v02"
+	"github.com/spf13/cobra"
+)
+
+type addRootPersonOptions struct {
+	p                    *persistent.Options
+	personID             string
+	publicKeys           []string
+	associatedIdentities []string
+	customMetadata       []string
+}
+
+func (o *addRootPersonOptions) AddFlags(cmd *cobra.Command) {
+	cmd.Flags().StringVar(
+		&o.personID,
+		"person-ID",
+		"",
+		"person ID",
+	)
+	cmd.MarkFlagRequired("person-ID") //nolint:errcheck
+
+	cmd.Flags().StringArrayVar(
+		&o.publicKeys,
+		"public-key",
+		[]string{},
+		"authorized public key for person",
+	)
+	cmd.MarkFlagRequired("public-key") //nolint:errcheck
+
+	cmd.Flags().StringArrayVar(
+		&o.associatedIdentities,
+		"associated-identity",
+		[]string{},
+		fmt.Sprintf("identities on code review platforms in the form 'providerID::identity' (e.g., '%s::<username>+<user ID>')", tuf.GitHubAppRoleName),
+	)
+
+	cmd.Flags().StringArrayVar(
+		&o.customMetadata,
+		"custom",
+		[]string{},
+		"additional custom metadata in the form KEY=VALUE",
+	)
+}
+
+func (o *addRootPersonOptions) Run(cmd *cobra.Command, _ []string) error {
+	repo, err := gittuf.LoadRepository(".")
+	if err != nil {
+		return err
+	}
+
+	signer, err := gittuf.LoadSigner(repo, o.p.SigningKey)
+	if err != nil {
+		return err
+	}
+
+	publicKeys := map[string]*tufv02.Key{}
+	for _, key := range o.publicKeys {
+		key, err := gittuf.LoadPublicKey(key)
+		if err != nil {
+			return err
+		}
+
+		publicKeys[key.ID()] = key.(*tufv02.Key)
+	}
+
+	associatedIdentities := map[string]string{}
+	for _, associatedIdentity := range o.associatedIdentities {
+		split := strings.Split(associatedIdentity, "::")
+		if len(split) != 2 {
+			return fmt.Errorf("invalid format for associated identity '%s'", associatedIdentity)
+		}
+		associatedIdentities[split[0]] = split[1]
+	}
+
+	custom := map[string]string{}
+	for _, customEntry := range o.customMetadata {
+		split := strings.Split(customEntry, "=")
+		if len(split) != 2 {
+			return fmt.Errorf("invalid format for custom metadata '%s'", customEntry)
+		}
+		custom[split[0]] = split[1]
+	}
+
+	person := &tufv02.Person{
+		PersonID:             o.personID,
+		PublicKeys:           publicKeys,
+		AssociatedIdentities: associatedIdentities,
+		Custom:               custom,
+	}
+
+	opts := []trustpolicyopts.Option{}
+	if o.p.WithRSLEntry {
+		opts = append(opts, trustpolicyopts.WithRSLEntry())
+	}
+
+	return repo.AddRootKey(cmd.Context(), signer, person, true, opts...)
+}
+
+func newAddRootPersonCommand(p *persistent.Options) *cobra.Command {
+	o := &addRootPersonOptions{p: p}
+
+	cmd := &cobra.Command{
+		Use:               "add-root-person",
+		Short:             "Add a trusted person to the gittuf root of trust",
+		Long:              `The 'add-root-person' command allows users to add a new trusted person as a principal in the repository's root of trust. A person definition consists of a unique identifier ('--person-ID'), one or more authorized public keys ('--public-key'), optional associated identities ('--associated-identity') on external platforms (e.g., GitHub, GitLab), and optional custom metadata ('--custom') for tracking additional attributes. The keys can be specified from disk, from the GPG keyring using the "gpg:<fingerprint>" format, or as a Sigstore identity as "fulcio:<identity>::<issuer>".`,
+		RunE:              o.Run,
+		DisableAutoGenTag: true,
+	}
+
+	o.AddFlags(cmd)
+
+	return cmd
+}
