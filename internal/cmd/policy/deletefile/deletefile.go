8819d62b1b002f0681c8e797acbf30ab91377ebf
Xingyu Li <xl5936@nyu.edu>
2026-06-23 09:50:18 -0400

    policy: add delete-file command
    Signed-off-by: Xingyu Li <xl5936@nyu.edu>

---
+++ internal/cmd/policy/deletefile/deletefile.go
@@ -0,0 +1,51 @@
+package deletefile
+
+import (
+	"github.com/gittuf/gittuf/experimental/gittuf"
+	trustpolicyopts "github.com/gittuf/gittuf/experimental/gittuf/options/trustpolicy"
+	"github.com/gittuf/gittuf/internal/cmd/policy/persistent"
+	"github.com/gittuf/gittuf/internal/policy"
+	"github.com/spf13/cobra"
+)
+
+type options struct {
+	p          *persistent.Options
+	policyName string
+}
+
+func (o *options) AddFlags(cmd *cobra.Command) {
+	cmd.Flags().StringVar(
+		&o.policyName,
+		"policy-name",
+		policy.TargetsRoleName,
+		"name of delegated policy file to delete",
+	)
+}
+
+func (o *options) Run(cmd *cobra.Command, _ []string) error {
+	repo, err := gittuf.LoadRepository(".")
+	if err != nil {
+		return err
+	}
+
+	opts := []trustpolicyopts.Option{}
+	if o.p.WithRSLEntry {
+		opts = append(opts, trustpolicyopts.WithRSLEntry())
+	}
+
+	return repo.DeletePolicyFile(cmd.Context(), o.policyName, true, opts...)
+}
+
+func New(persistent *persistent.Options) *cobra.Command {
+	o := &options{p: persistent}
+	cmd := &cobra.Command{
+		Use:               "delete-file",
+		Short:             "Delete a delegated policy file",
+		Long:              "The 'delete-file' command removes a delegated gittuf policy file from policy metadata.",
+		RunE:              o.Run,
+		DisableAutoGenTag: true,
+	}
+	o.AddFlags(cmd)
+
+	return cmd
+}
